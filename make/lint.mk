a739ef74a427ab2ae95d4784afab7a213c8343bc
Dipak Pawar <dipakpawar231@gmail.com>
2019-06-12 19:45:32 +0530

    chore: introduce make targets required for openshift-ci (#3)

---
+++ make/lint.mk
@@ -0,0 +1,30 @@
+ifndef LINT_MK
+LINT_MK:=# Prevent repeated "-include".
+
+GOLANGCI_LINT_BIN=./out/golangci-lint
+
+include ./make/verbose.mk
+include ./make/go.mk
+
+.PHONY: lint
+## Runs linters on Go code files and YAML files
+lint: lint-go-code lint-yaml
+
+YAML_FILES := $(shell find . -type f -regex ".*y[a]ml" -print)
+.PHONY: lint-yaml
+## runs yamllint on all yaml files
+lint-yaml: ./vendor ${YAML_FILES}
+	$(Q)yamllint -c .yamllint $(YAML_FILES)
+
+.PHONY: lint-go-code
+## Checks the code with golangci-lint
+lint-go-code: $(GOLANGCI_LINT_BIN)
+	# This is required for OpenShift CI enviroment
+	# Ref: https://github.com/openshift/release/pull/3438#issuecomment-482053250
+	$(Q)GOCACHE=$(shell pwd)/out/gocache ./out/golangci-lint ${V_FLAG} run --deadline=10m
+
+$(GOLANGCI_LINT_BIN):
+	$(Q)curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ./out v1.17.1
+
+endif
+

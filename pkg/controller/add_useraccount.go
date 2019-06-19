613a898c6bd375c7943a50e11cd7e2fc3f47210c
Nurali Virani <nurali.techie@gmail.com>
2019-06-19 15:35:21 +0530

    useraccount ctrl added with noop (#17)
    * useraccount ctrl added with noop
    
    * minor change: comments
    
    * minor change: comments remvoed

---
+++ pkg/controller/add_useraccount.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/codeready-toolchain/member-operator/pkg/controller/useraccount"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, useraccount.Add)
+}

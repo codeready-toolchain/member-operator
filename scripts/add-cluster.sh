acf88f0ae0b2c150fd39e2cc630613b629ad7561
Matous Jobanek <mjobanek@redhat.com>
2019-06-20 10:58:54 +0200

    use correct ns name in script

--- scripts/add-cluster.sh
+++ scripts/add-cluster.sh
@@ -13,7 +13,8 @@ fi
 
 echo "Switching to profile ${JOINING_CLUSTER_TYPE}"
 minishift profile set ${JOINING_CLUSTER_TYPE}
-make use-namespace
+make login-as-admin
+oc project "toolchain-${JOINING_CLUSTER_TYPE}-operator"
 
 echo "Getting ${JOINING_CLUSTER_TYPE} SA token"
 SA_SECRET=`oc get sa ${SA_NAME} -o json | jq -r .secrets[].name | grep token`
@@ -24,7 +25,8 @@ API_ENDPOINT=`oc config view --raw --minify -o json | jq -r '.clusters[0].cluste
 
 echo "Switching to profile ${CLUSTER_JOIN_TO}"
 minishift profile set ${CLUSTER_JOIN_TO}
-make use-namespace
+make login-as-admin
+oc project "toolchain-${CLUSTER_JOIN_TO}-operator"
 
 oc create secret generic ${SA_NAME}-${JOINING_CLUSTER_NAME} --from-literal=token="${SA_TOKEN}" --from-literal=ca.crt="${SA_CA_CRT}"
 

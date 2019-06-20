317256be986e741e602b8a7a7d265c86fae50cd0
Matous Jobanek <matousjobanek@gmail.com>
2019-06-20 08:11:39 +0200

    Update make/dev.mk
    Co-Authored-By: Alexey Kazakov <alkazako@redhat.com>

--- make/dev.mk
+++ make/dev.mk
@@ -61,7 +61,7 @@ deploy-rbac:
 ## Deploy CRD
 deploy-crd:
 	$(Q)-oc apply -f deploy/crds/toolchain_v1alpha1_nstemplateset.yaml
-	$(Q)-oc apply -f deploy/crds/toolchain_v1alpha1_useraccount_crd.yaml
+	$(Q)-oc apply -f deploy/crds/toolchain_v1alpha1_useraccount.yaml
 	$(Q)-oc apply -f deploy/crds/core_v1beta1_kubefedcluster.yaml
 
 .PHONY: add-member-to-host

0266c7f2b7c2268921eddd17bfadc6f721843372
Alexey Kazakov <alkazako@redhat.com>
2019-06-26 22:47:33 -0700

    More tests

--- pkg/controller/useraccount/useraccount_controller_test.go
+++ pkg/controller/useraccount/useraccount_controller_test.go
@@ -48,6 +48,33 @@ func TestReconcileOK(t *testing.T) {
 	identity := &userv1.Identity{}
 	user := &userv1.User{}
 
+	t.Run("deleted_account_ignored", func(t *testing.T) {
+		// given
+		// No user account exists
+		objs := []runtime.Object{}
+		client := fake.NewFakeClient(objs...)
+		r := &ReconcileUserAccount{
+			client: client,
+			scheme: s,
+		}
+		//when
+		res, err := r.Reconcile(req)
+
+		//then
+		require.NoError(t, err)
+		assert.Equal(t, reconcile.Result{}, res)
+
+		// Check the user is not created
+		err = r.client.Get(context.TODO(), types.NamespacedName{Name: userAcc.Name}, user)
+		require.Error(t, err)
+		assert.True(t, errors.IsNotFound(err))
+
+		// Check the identity is not created
+		err = r.client.Get(context.TODO(), types.NamespacedName{Name: getIdentityName(userAcc)}, identity)
+		require.Error(t, err)
+		assert.True(t, errors.IsNotFound(err))
+	})
+
 	t.Run("create_user", func(t *testing.T) {
 		//when
 		res, err := r.Reconcile(req)
@@ -66,6 +93,8 @@ func TestReconcileOK(t *testing.T) {
 		err = r.client.Get(context.TODO(), types.NamespacedName{Name: userAcc.Name}, user)
 		require.NoError(t, err)
 		assert.Equal(t, userAcc.Name, user.Name)
+		assert.Len(t, user.GetOwnerReferences(), 1)
+		assert.Equal(t, updatedAcc.UID, user.GetOwnerReferences()[0].UID)
 
 		// Check the identity is not created yet
 		err = r.client.Get(context.TODO(), types.NamespacedName{Name: getIdentityName(userAcc)}, identity)
@@ -91,6 +120,8 @@ func TestReconcileOK(t *testing.T) {
 		err = r.client.Get(context.TODO(), types.NamespacedName{Name: getIdentityName(userAcc)}, identity)
 		require.NoError(t, err)
 		assert.Equal(t, fmt.Sprintf("%s:%s", config.GetIdP(), userAcc.Spec.UserID), identity.Name)
+		assert.Len(t, identity.GetOwnerReferences(), 1)
+		assert.Equal(t, updatedAcc.UID, identity.GetOwnerReferences()[0].UID)
 	})
 
 	t.Run("create_user_identity_mapping", func(t *testing.T) {
@@ -130,84 +161,6 @@ func TestReconcileOK(t *testing.T) {
 	})
 }
 
-func TestEnsureIdentity(t *testing.T) {
-	//logf.SetLogger(logf.ZapLogger(true))
-	//username := "johnsmith"
-	//userID := "34113f6a-2e0d-11e9-be9a-525400fb443d"
-	//s := scheme.Scheme
-	//err := apis.AddToScheme(s)
-	//require.NoError(t, err)
-	//
-	//t.Run("create_identity", func(t *testing.T) {
-	//	userAcc := newUserAccount(username, userID)
-	//	client := fake.NewFakeClient()
-	//	reconciler := &ReconcileUserAccount{
-	//		client: client,
-	//		scheme: s,
-	//	}
-	//
-	//	newIdentity, created, err := reconciler.ensureIdentity(userAcc)
-	//	assert.NoError(t, err)
-	//	assert.True(t, created)
-	//	assert.NotNil(t, newIdentity)
-	//})
-	//
-	//t.Run("get_identity", func(t *testing.T) {
-	//	userAcc := newUserAccount(username, userID)
-	//	identity := newIdentity(userAcc)
-	//	client := fake.NewFakeClient(identity)
-	//	reconciler := &ReconcileUserAccount{
-	//		client: client,
-	//		scheme: s,
-	//	}
-	//
-	//	newIdentity, created, err := reconciler.ensureIdentity(userAcc)
-	//	assert.NoError(t, err)
-	//	assert.False(t, created)
-	//	assert.NotNil(t, newIdentity)
-	//})
-}
-
-func TestEnsureMapping(t *testing.T) {
-	//logf.SetLogger(logf.ZapLogger(true))
-	//username := "johnsmith"
-	//userID := "34113f6a-2e0d-11e9-be9a-525400fb443d"
-	//s := scheme.Scheme
-	//err := apis.AddToScheme(s)
-	//require.NoError(t, err)
-	//
-	//t.Run("create_mapping_true", func(t *testing.T) {
-	//	userAcc := newUserAccount(username, userID)
-	//	user := newUser(userAcc)
-	//	identity := newIdentity(userAcc)
-	//	client := fake.NewFakeClient(user, identity)
-	//	reconciler := &ReconcileUserAccount{
-	//		client: client,
-	//		scheme: s,
-	//	}
-	//
-	//	created, err := reconciler.ensureMapping(userAcc, user, identity)
-	//	assert.NoError(t, err)
-	//	assert.True(t, created)
-	//})
-	//
-	//t.Run("create_mapping_false", func(t *testing.T) {
-	//	userAcc := newUserAccount(username, userID)
-	//	user := newUser(userAcc)
-	//	identity := newIdentity(userAcc)
-	//	mapping := newMapping(user, identity)
-	//	client := fake.NewFakeClient(user, identity, mapping)
-	//
-	//	reconciler := &ReconcileUserAccount{
-	//		client: client,
-	//		scheme: s,
-	//	}
-	//	created, err := reconciler.ensureMapping(userAcc, user, identity)
-	//	assert.NoError(t, err)
-	//	assert.False(t, created)
-	//})
-}
-
 func TestUpdateStatus(t *testing.T) {
 	logf.SetLogger(logf.ZapLogger(true))
 	username := "johnsmith"
@@ -244,6 +197,23 @@ func TestUpdateStatus(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, toolchainv1alpha1.StatusProvisioning, userAcc.Status.Status)
 		assert.Equal(t, "some error", userAcc.Status.Error)
+	})
+
+	t.Run("status_error", func(t *testing.T) {
+		userAcc := newUserAccount(username, userID)
+		client := fake.NewFakeClient(userAcc)
+		reconciler := &ReconcileUserAccount{
+			client: client,
+			scheme: s,
+		}
+
+		log := logf.Log.WithName("test")
+		err := reconciler.wrapErrorWithStatusUpdate(log, userAcc, errors.NewBadRequest("oopsy woopsy"), "failed to create %s", "user bob")
+
+		require.Error(t, err)
+		assert.Equal(t, toolchainv1alpha1.StatusProvisioning, userAcc.Status.Status)
+		assert.Equal(t, "oopsy woopsy", userAcc.Status.Error)
+		assert.Equal(t, "failed to create user bob: oopsy woopsy", err.Error())
 	})
 }
 

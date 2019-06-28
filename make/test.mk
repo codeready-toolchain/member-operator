0c27a0eead9928d31c4f4c7d1efa6bc576b9d446
Xavier Coulon <xcoulon@redhat.com>
2019-06-28 15:14:58 +0200

    fix goals...
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/test.mk
+++ make/test.mk
@@ -1,15 +1,24 @@
 .PHONY: test
+## runs the tests without coverage and excluding E2E tests
+test:
+	@echo "running the tests without coverage and excluding E2E tests..."
+	$(Q)go test ${V_FLAG} -race $(shell go list ./... | grep -v /test/e2e) -failfast
+	
+.PHONY: test-ci
+# runs the tests and uploads the coverage report on codecov.io
+# DO NOT USE LOCALLY: must only be called by OpenShift CI when processing new PR and when a PR is merged! 
+test-ci: test-with-coverage upload-codecov-report
+
+# Output directory for coverage information
+COV_DIR = $(OUT_DIR)/coverage
+
+.PHONY: test-with-coverage
 ## runs the tests *with* coverage
-test: 
+test-with-coverage: 
 	@echo "running the tests with coverage..."
 	@-mkdir -p $(COV_DIR)
 	@-rm $(COV_DIR)/coverage.txt
 	$(Q)go test -vet off ${V_FLAG} $(shell go list ./... | grep -v /test/e2e) -coverprofile=$(COV_DIR)/coverage.txt -covermode=atomic ./...
-
-.PHONY: test-ci
-# runs the tests and uploads the coverage report on codecov.io
-# DO NOT USE LOCALLY: must only be called by OpenShift CI when processing new PR and when a PR is merged! 
-test-ci: test upload-codecov-report
 
 .PHONY: upload-codecov-report
 # Uploads the test coverage reports to codecov.io. 
@@ -17,12 +26,3 @@ test-ci: test upload-codecov-report
 upload-codecov-report: 
 	# Upload coverage to codecov.io
 	bash <(curl -s https://codecov.io/bash) -f $(COV_DIR)/coverage.txt -t 51cc45ad-2e54-4e68-98cb-8ab15cf2b8df
-
-# Output directory for coverage information
-COV_DIR = $(OUT_DIR)/coverage
-
-.PHONY: test-without-coverage
-## runs the tests without coverage and excluding E2E tests
-test-without-coverage:
-	@echo "running the tests without coverage and excluding E2E tests..."
-	$(Q)go test ${V_FLAG} -race $(shell go list ./... | grep -v /test/e2e) -failfast

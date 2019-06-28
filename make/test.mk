8da341c4f82a1f9127a741594d3d2a7e9c5a9fc8
Xavier Coulon <xcoulon@redhat.com>
2019-06-28 10:32:57 +0200

    new make goal to run the tests and upload cov report on CI
    we should not upload coverage reports during local testing
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/test.mk
+++ make/test.mk
@@ -2,13 +2,19 @@
 ## runs the tests *with* coverage
 test: 
 	@echo "running the tests with coverage..."
-	-mkdir -p $(COV_DIR)
-	-rm $(COV_DIR)/coverage.txt
-	$(Q)go test -vet off ${V_FLAG} $(shell go list ./... | grep -v /test/e2e) -coverprofile=$(COV_DIR)/profile.out -covermode=atomic ./...
-ifeq (,$(wildcard $(COV_DIR)/profile.out))
-	cat $(COV_DIR)/profile.out >> $(COV_DIR)/coverage.txt
-	rm $(COV_DIR)/profile.out
-endif
+	@-mkdir -p $(COV_DIR)
+	@-rm $(COV_DIR)/coverage.txt
+	$(Q)go test -vet off ${V_FLAG} $(shell go list ./... | grep -v /test/e2e) -coverprofile=$(COV_DIR)/coverage.txt -covermode=atomic ./...
+
+.PHONY: test-ci
+# runs the tests and uploads the coverage report on codecov.io
+# DO NOT USE LOCALLY: must only be called by OpenShift CI when processing new PR and when a PR is merged! 
+test-ci: test upload-codecov-report
+
+.PHONY: upload-codecov-report
+# Uploads the test coverage reports to codecov.io. 
+# DO NOT USE LOCALLY: must only be called by OpenShift CI when processing new PR and when a PR is merged! 
+upload-codecov-report: 
 	# Upload coverage to codecov.io
 	bash <(curl -s https://codecov.io/bash) -f $(COV_DIR)/coverage.txt -t 51cc45ad-2e54-4e68-98cb-8ab15cf2b8df
 

61322bedcd6b2a5216432fd9b7aa7c92bc0357fe
nurali-techie <nurali.techie@gmail.com>
2019-07-01 18:02:19 +0530

    create user test added

--- test/e2e/useraccount_test.go
+++ test/e2e/useraccount_test.go
@@ -1,22 +1,18 @@
 package e2e
 
 import (
+	"context"
+	"fmt"
 	"testing"
-	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/member-operator/pkg/apis"
+	"github.com/codeready-toolchain/member-operator/pkg/config"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-const (
-	retryInterval        = time.Second * 5
-	timeout              = time.Second * 60
-	cleanupRetryInterval = time.Second * 1
-	cleanupTimeout       = time.Second * 5
 )
 
 func TestUserAccount(t *testing.T) {
@@ -43,9 +39,58 @@ func TestUserAccount(t *testing.T) {
 	// get global framework variables
 	f := framework.Global
 
-	// wait for toolchain-operator to be ready
+	// wait for operator to be ready
 	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "member-operator", 1, retryInterval, timeout)
 	require.NoError(t, err, "failed while waiting for operator deployment")
 
 	t.Log("member-operator is ready and running state")
+
+	userAcc := createUserAcc(t, f, ctx)
+	err = verifyResources(t, f, userAcc)
+	assert.NoError(t, err)
+}
+
+func createUserAcc(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) *toolchainv1alpha1.UserAccount {
+	name := "johnsmith"
+	userAcc := &toolchainv1alpha1.UserAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "toolchain-member-operator",
+		},
+		Spec: toolchainv1alpha1.UserAccountSpec{
+			UserID:  "1a03ecac-7c0b-44fc-b66d-12dd7fb21c40",
+			NSLimit: "admin",
+			NSTemplateSet: toolchainv1alpha1.NSTemplateSetSpec{
+				TierName: "basic",
+				Namespaces: []toolchainv1alpha1.Namespace{
+					toolchainv1alpha1.Namespace{Type: "ide", Revision: "ab12ef"},
+					toolchainv1alpha1.Namespace{Type: "cicd", Revision: "34efcd"},
+					toolchainv1alpha1.Namespace{Type: "stage", Revision: "cdef56"},
+				},
+			},
+		},
+	}
+
+	err := f.Client.Create(context.TODO(), userAcc, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	require.NoError(t, err)
+
+	t.Logf("user account '%s' created", name)
+	return userAcc
+}
+
+func verifyResources(t *testing.T, f *framework.Framework, userAcc *toolchainv1alpha1.UserAccount) error {
+	if err := waitForUser(t, f.Client.Client, userAcc.Name); err != nil {
+		return err
+	}
+	if err := waitForIdentity(t, f.Client.Client, getIdentityName(userAcc)); err != nil {
+		return err
+	}
+	if err := waitForMapping(t, f.Client.Client, userAcc.Name, getIdentityName(userAcc)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getIdentityName(userAcc *toolchainv1alpha1.UserAccount) string {
+	return fmt.Sprintf("%s:%s", config.GetIdP(), userAcc.Spec.UserID)
 }
---
+++ test/e2e/wait_util.go
@@ -0,0 +1,77 @@
+package e2e
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	userv1 "github.com/openshift/api/user/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	retryInterval        = time.Second * 5
+	timeout              = time.Second * 60
+	cleanupRetryInterval = time.Second * 1
+	cleanupTimeout       = time.Second * 5
+)
+
+func waitForUser(t *testing.T, client client.Client, name string) error {
+	return wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		user := &userv1.User{}
+		if err := client.Get(context.TODO(), types.NamespacedName{Name: name}, user); err != nil {
+			if errors.IsNotFound(err) {
+				t.Logf("waiting for availability of user '%s'\n", name)
+				return false, nil
+			}
+			return false, err
+		}
+		if user != nil {
+			t.Logf("found user '%s'\n", name)
+			return true, nil
+		}
+		return false, nil
+	})
+}
+
+func waitForIdentity(t *testing.T, client client.Client, name string) error {
+	return wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		identity := &userv1.Identity{}
+		if err := client.Get(context.TODO(), types.NamespacedName{Name: name}, identity); err != nil {
+			if errors.IsNotFound(err) {
+				t.Logf("waiting for availability of identity '%s'\n", name)
+				return false, nil
+			}
+			return false, err
+		}
+		if identity != nil {
+			t.Logf("found identity '%s'\n", name)
+			return true, nil
+		}
+		return false, nil
+	})
+}
+
+func waitForMapping(t *testing.T, client client.Client, userName, identityName string) error {
+	return wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		identity := &userv1.UserIdentityMapping{}
+		if err := client.Get(context.TODO(), types.NamespacedName{Name: identityName}, identity); err != nil {
+			if errors.IsNotFound(err) {
+				t.Logf("waiting for availability of identity '%s'\n", identityName)
+				return false, nil
+			}
+			return false, err
+		}
+		if identity != nil {
+			if identity.User.Name == userName {
+				t.Logf("found mapping between user '%s' and identity '%s'\n", userName, identityName)
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+}

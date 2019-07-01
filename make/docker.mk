3d6ad2d2c2425101253d398aa7cb5315f819cad3
Xavier Coulon <xcoulon@redhat.com>
2019-07-01 15:34:13 +0200

    chore: add 'make test-e2e'
    the operator is deployed in a separate project
    which is re-created on the fly, with all the required
    CRDs, role and rolebinding.
    
    the tests are then executed with the `operator-sdk test local`
    command
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ make/docker.mk
@@ -0,0 +1,12 @@
+.PHONY: docker-image
+## Build the docker image that can be deployed (only contains bare operator)
+docker-image: ./build/Dockerfile build
+	$(Q)(eval $$(minishift oc-env)) && \
+	(eval $$(minishift docker-env)) && \
+	docker build ${Q_FLAG} \
+		--build-arg GO_PACKAGE_PATH=${GO_PACKAGE_PATH} \
+		--build-arg VERBOSE=${VERBOSE} \
+		-f build/Dockerfile \
+		-t ${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT} \
+		. \
+	
--- make/test.mk
+++ make/test.mk
@@ -1,9 +1,21 @@
+############################################################
+#
+# (local) Tests
+#
+############################################################
+
 .PHONY: test
 ## runs the tests without coverage and excluding E2E tests
 test:
 	@echo "running the tests without coverage and excluding E2E tests..."
 	$(Q)go test ${V_FLAG} -race $(shell go list ./... | grep -v /test/e2e) -failfast
 	
+############################################################
+#
+# OpenShift CI Tests
+#
+############################################################
+
 .PHONY: test-ci
 # runs the tests and uploads the coverage report on codecov.io
 # DO NOT USE LOCALLY: must only be called by OpenShift CI when processing new PR and when a PR is merged! 
@@ -13,7 +25,7 @@ test-ci: test-with-coverage upload-codecov-report
 COV_DIR = $(OUT_DIR)/coverage
 
 .PHONY: test-with-coverage
-## runs the tests *with* coverage
+## runs the tests with coverage
 test-with-coverage: 
 	@echo "running the tests with coverage..."
 	@-mkdir -p $(COV_DIR)
@@ -26,3 +38,41 @@ test-with-coverage:
 upload-codecov-report: 
 	# Upload coverage to codecov.io
 	bash <(curl -s https://codecov.io/bash) -f $(COV_DIR)/coverage.txt -t 51cc45ad-2e54-4e68-98cb-8ab15cf2b8df
+
+############################################################
+#
+# End-to-end Tests
+#
+############################################################
+
+.PHONY: test-e2e
+## Runs the e2e tests locally
+test-e2e: e2e-setup 
+	$(info Running E2E test: $@)
+ifeq ($(OPENSHIFT_VERSION),3)
+	$(Q)oc login -u system:admin
+endif
+	$(Q)operator-sdk test local ./test/e2e --namespace $(TEST_NAMESPACE) --up-local --go-test-flags "-v -timeout=15m"
+
+e2e-setup: ./vendor get-test-namespace e2e-cleanup docker-image
+	$(Q)oc new-project $(TEST_NAMESPACE) --display-name e2e-tests
+	$(Q)-oc apply -f ./deploy/service_account.yaml 
+	$(Q)-oc apply -f ./deploy/role.yaml 
+	$(Q)sed -e "s,REPLACE_NAMESPACE,$(TEST_NAMESPACE)," ./deploy/role_binding.yaml | oc apply -f -
+	$(foreach crd_file,$(wildcard deploy/crds/*.yaml), \
+		oc apply -f $(crd_file); \
+	)
+	$(eval OPERATOR_IMAGE_NAME := $(shell minishift openshift registry)/${TEST_NAMESPACE}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT})
+	$(Q)docker tag ${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT} $(OPERATOR_IMAGE_NAME)
+	$(Q)eval `minishift docker-env` && docker login -u admin -p $(shell oc whoami -t) $(shell minishift openshift registry) && docker push $(OPERATOR_IMAGE_NAME)
+	$(Q)sed -e "s,REPLACE_IMAGE,$(OPERATOR_IMAGE_NAME)," ./deploy/operator.yaml | oc apply -f -
+
+e2e-cleanup: get-test-namespace
+	$(Q)-oc delete project $(TEST_NAMESPACE) --timeout=10s --wait
+
+get-test-namespace: $(OUT_DIR)/test-namespace
+	$(eval TEST_NAMESPACE := $(shell cat $(OUT_DIR)/test-namespace))
+
+$(OUT_DIR)/test-namespace:
+	@echo -n "test-namespace-$(shell uuidgen | tr '[:upper:]' '[:lower:]')" > $(OUT_DIR)/test-namespace
+

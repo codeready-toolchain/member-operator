7aff7d45d4ae4dfa72e45182876e20d3be83936a
nurali-techie <nurali.techie@gmail.com>
2019-07-03 11:26:06 +0530

    delete test added for useraccount

--- test/e2e/useraccount_test.go
+++ test/e2e/useraccount_test.go
@@ -8,20 +8,17 @@ import (
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/member-operator/pkg/apis"
 	"github.com/codeready-toolchain/member-operator/pkg/config"
+	userv1 "github.com/openshift/api/user/v1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestUserAccount(t *testing.T) {
-	userAccList := &toolchainv1alpha1.UserAccountList{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "UserAccount",
-			APIVersion: "toolchain.dev.openshift.com/v1alpha1",
-		},
-	}
+	userAccList := &toolchainv1alpha1.UserAccountList{}
 
 	err := framework.AddToFrameworkScheme(apis.AddToScheme, userAccList)
 	require.NoError(t, err, "failed to add custom resource scheme to framework")
@@ -38,6 +35,7 @@ func TestUserAccount(t *testing.T) {
 
 	// get global framework variables
 	f := framework.Global
+	client := f.Client.Client
 
 	// wait for operator to be ready
 	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "member-operator", 1, retryInterval, timeout)
@@ -45,12 +43,43 @@ func TestUserAccount(t *testing.T) {
 
 	t.Log("member-operator is ready and running state")
 
-	userAcc := createUserAcc(t, f, ctx)
-	err = verifyResources(t, f, userAcc)
-	assert.NoError(t, err)
+	// create useraccount
+	userAcc := newUserAcc(t, f, ctx)
+	err = f.Client.Create(context.TODO(), userAcc, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	require.NoError(t, err)
+	t.Logf("user account '%s' created", userAcc.Name)
+
+	t.Run("verify_useraccount_ok", func(t *testing.T) {
+		err := verifyResources(t, f, userAcc)
+		assert.NoError(t, err)
+	})
+
+	t.Run("delete_user_ok", func(t *testing.T) {
+		user := &userv1.User{}
+		err := client.Get(context.TODO(), types.NamespacedName{Name: userAcc.Name}, user)
+		require.NoError(t, err)
+
+		err = client.Delete(context.TODO(), user)
+		require.NoError(t, err)
+
+		err = verifyResources(t, f, userAcc)
+		assert.NoError(t, err)
+	})
+
+	t.Run("delete_identity_ok", func(t *testing.T) {
+		identity := &userv1.Identity{}
+		err := client.Get(context.TODO(), types.NamespacedName{Name: getIdentityName(userAcc)}, identity)
+		require.NoError(t, err)
+
+		err = client.Delete(context.TODO(), identity)
+		require.NoError(t, err)
+
+		err = verifyResources(t, f, userAcc)
+		assert.NoError(t, err)
+	})
 }
 
-func createUserAcc(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) *toolchainv1alpha1.UserAccount {
+func newUserAcc(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) *toolchainv1alpha1.UserAccount {
 	name := "johnsmith"
 	userAcc := &toolchainv1alpha1.UserAccount{
 		ObjectMeta: metav1.ObjectMeta{
@@ -70,11 +99,6 @@ func createUserAcc(t *testing.T, f *framework.Framework, ctx *framework.TestCtx)
 			},
 		},
 	}
-
-	err := f.Client.Create(context.TODO(), userAcc, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
-	require.NoError(t, err)
-
-	t.Logf("user account '%s' created", name)
 	return userAcc
 }
 

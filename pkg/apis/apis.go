ac737457fa4d39c049ee7faeb7d0f98eec5fcf29
Matous Jobanek <mjobanek@redhat.com>
2019-08-12 11:53:10 +0200

    fix: use scheme with registered types from api repo

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,17 +1,15 @@
 package apis
 
 import (
+	"github.com/codeready-toolchain/api/pkg/apis"
 	userv1 "github.com/openshift/api/user/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// AddToSchemes may be used to add all resources defined in the project to a Scheme
-var AddToSchemes runtime.SchemeBuilder
-
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
 	// add openshift specific resource
-	AddToSchemes = append(AddToSchemes, userv1.AddToScheme)
+	AddToSchemes := append(apis.AddToSchemes, userv1.AddToScheme)
 
 	return AddToSchemes.AddToScheme(s)
 }

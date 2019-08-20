9ca0a8eacde819f6b7089d46b0987bc8cb3fb8c4
Dipak Pawar <dipakpawar231@gmail.com>
2019-08-20 12:28:33 +0530

    revert useraccount_controller.go to master

--- pkg/controller/useraccount/useraccount_controller.go
+++ pkg/controller/useraccount/useraccount_controller.go
@@ -34,7 +34,6 @@ const (
 	unableToCreateUserReason     = "UnableToCreateUser"
 	unableToCreateIdentityReason = "UnableToCreateIdentity"
 	unableToCreateMappingReason  = "UnableToCreateMapping"
-	unableToCreateNSTemplateSet  = "UnableToCreateNSTemplateSet"
 	provisioningReason           = "Provisioning"
 	provisionedReason            = "Provisioned"
 
@@ -75,9 +74,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 	if err := c.Watch(&source.Kind{Type: &userv1.Identity{}}, enqueueRequestForOwner); err != nil {
-		return err
-	}
-	if err := c.Watch(&source.Kind{Type: &toolchainv1alpha1.NSTemplateSet{}}, enqueueRequestForOwner); err != nil {
 		return err
 	}
 
@@ -133,18 +129,11 @@ func (r *ReconcileUserAccount) Reconcile(request reconcile.Request) (reconcile.R
 		if _, createdOrUpdated, err = r.ensureIdentity(reqLogger, userAcc, user); err != nil || createdOrUpdated {
 			return reconcile.Result{}, err
 		}
-
-		if createdOrUpdated, err = r.ensureNSTemplateSet(reqLogger, userAcc); err != nil || createdOrUpdated {
-			return reconcile.Result{}, err
-		}
 	} else if util.HasFinalizer(userAcc, userAccFinalizerName) {
 		if err = r.manageCleanUpLogic(reqLogger, userAcc); err != nil {
 			return reconcile.Result{}, err
 		}
 	}
-
-
-
 	return reconcile.Result{}, r.setStatusReady(userAcc)
 }
 
@@ -239,48 +228,6 @@ func (r *ReconcileUserAccount) ensureIdentity(logger logr.Logger, userAcc *toolc
 	}
 
 	return identity, false, nil
-}
-
-// TODO introduce new status
-func (r *ReconcileUserAccount) ensureNSTemplateSet(logger logr.Logger, userAcc *toolchainv1alpha1.UserAccount) (bool, error) {
-	name := ToNSTemplateSetName(userAcc.Name)
-	if userAcc.Spec.NSTemplateSetName.Name == "" {
-		if err := r.setStatusProvisioning(userAcc); err != nil {
-			return false, err
-		}
-		userAcc.Spec.NSTemplateSetName = corev1.LocalObjectReference{Name: name}
-		if err := r.client.Update(context.TODO(), userAcc); err != nil {
-			return false, r.wrapErrorWithStatusUpdate(logger, userAcc, r.setStatusNSTemplateSetCreationFailed, err,
-				"failed to update UserAccount '%s' with NSTemplateSet", name)
-		}
-		logger.Info("UserAccount updated with NSTemplateSet", "name", name)
-		return true, nil
-	}
-
-	nsTeplSet := &toolchainv1alpha1.NSTemplateSet{}
-	if err := r.client.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: userAcc.Namespace}, nsTeplSet); err != nil {
-		if errors.IsNotFound(err) {
-			if err := r.setStatusProvisioning(userAcc); err != nil {
-				return false, err
-			}
-			nsTeplSet = newNSTemplateSet(userAcc)
-			if err := controllerutil.SetControllerReference(userAcc, nsTeplSet, r.scheme); err != nil {
-				return false, err
-			}
-			if err = r.client.Create(context.TODO(), nsTeplSet); err != nil {
-				return false, r.wrapErrorWithStatusUpdate(logger, userAcc, r.setStatusNSTemplateSetCreationFailed, err,
-					"failed to create NSTemplateSet '%s'", name)
-			}
-			logger.Info("NSTemplateSet created for UserAccount", "name", name)
-			return true, nil
-		}
-	}
-
-	if nsTeplSet.Spec.TierName != userAcc.Spec.NSTemplateSet.TierName {
-		// TODO tier is changed, do needful
-	}
-
-	return false, nil
 }
 
 // setFinalizers sets the finalizers for UserAccount
@@ -403,17 +350,6 @@ func (r *ReconcileUserAccount) setStatusMappingCreationFailed(userAcc *toolchain
 		})
 }
 
-func (r *ReconcileUserAccount) setStatusNSTemplateSetCreationFailed(userAcc *toolchainv1alpha1.UserAccount, message string) error {
-	return r.updateStatusConditions(
-		userAcc,
-		toolchainv1alpha1.Condition{
-			Type:    toolchainv1alpha1.ConditionReady,
-			Status:  corev1.ConditionFalse,
-			Reason:  unableToCreateNSTemplateSet,
-			Message: message,
-		})
-}
-
 func (r *ReconcileUserAccount) setStatusProvisioning(userAcc *toolchainv1alpha1.UserAccount) error {
 	return r.updateStatusConditions(
 		userAcc,
@@ -471,25 +407,6 @@ func newIdentity(userAcc *toolchainv1alpha1.UserAccount, user *userv1.User) *use
 	return identity
 }
 
-func newNSTemplateSet(userAcc *toolchainv1alpha1.UserAccount) *toolchainv1alpha1.NSTemplateSet {
-	name := ToNSTemplateSetName(userAcc.Name)
-	nsTeplSet := &toolchainv1alpha1.NSTemplateSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: userAcc.Namespace,
-		},
-		Spec: toolchainv1alpha1.NSTemplateSetSpec{
-			TierName:   userAcc.Spec.TierName,
-			Namespaces: []toolchainv1alpha1.Namespace{},
-		},
-	}
-	return nsTeplSet
-}
-
 func ToIdentityName(userID string) string {
 	return fmt.Sprintf("%s:%s", config.GetIdP(), userID)
-}
-
-func ToNSTemplateSetName(name string) string {
-	return name
 }

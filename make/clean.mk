cce837020a53edb9a198ef415f6fcfe2cfe7e1a2
Dipak Pawar <dipakpawar231@gmail.com>
2019-08-23 19:13:34 +0530

    cleanup make target

--- make/clean.mk
+++ make/clean.mk
@@ -2,4 +2,3 @@
 clean:
 	$(Q)-rm -rf ${V_FLAG} $(OUT_DIR) ./vendor
 	$(Q)go clean ${X_FLAG} ./...
-	$(Q)-rm -rf test/templates/template_data.go
--- make/test.mk
+++ make/test.mk
@@ -21,7 +21,7 @@ COV_DIR = $(OUT_DIR)/coverage
 
 .PHONY: test-with-coverage
 ## runs the tests with coverage
-test-with-coverage:
+test-with-coverage: 
 	@echo "running the tests with coverage..."
 	@-mkdir -p $(COV_DIR)
 	@-rm $(COV_DIR)/coverage.txt

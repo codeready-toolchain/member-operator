b5331a71971d1100602a80df0724a9200d65ffb7
nurali-techie <nurali.techie@gmail.com>
2019-08-23 14:01:42 +0530

    minor chnage - minor refactoring

--- pkg/controller/useraccount/useraccount_controller.go
+++ pkg/controller/useraccount/useraccount_controller.go
@@ -352,35 +352,6 @@ func (r *ReconcileUserAccount) deleteIdentity(logger logr.Logger, userAcc *toolc
 	return nil, true
 }
 
-func compareNSTemplateSet(first, second toolchainv1alpha1.NSTemplateSetSpec) bool {
-	if first.TierName != second.TierName {
-		return false
-	}
-	return compareNamespaces(first.Namespaces, second.Namespaces)
-}
-
-func compareNamespaces(namespaces1, namespaces2 []toolchainv1alpha1.Namespace) bool {
-	if len(namespaces1) != len(namespaces2) {
-		return false
-	}
-	for _, ns1 := range namespaces1 {
-		found := findNamespace(ns1, namespaces2)
-		if !found {
-			return false
-		}
-	}
-	return true
-}
-
-func findNamespace(thisNs toolchainv1alpha1.Namespace, namespaces []toolchainv1alpha1.Namespace) bool {
-	for _, ns := range namespaces {
-		if ns.Type == thisNs.Type && ns.Revision == thisNs.Revision && ns.Template == thisNs.Template {
-			return true
-		}
-	}
-	return false
-}
-
 // wrapErrorWithStatusUpdate wraps the error and update the user account status. If the update failed then logs the error.
 func (r *ReconcileUserAccount) wrapErrorWithStatusUpdate(logger logr.Logger, userAcc *toolchainv1alpha1.UserAccount, statusUpdater func(userAcc *toolchainv1alpha1.UserAccount, message string) error, err error, format string, args ...interface{}) error {
 	if err == nil {

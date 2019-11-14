2ce64bcf14a83f5f3bcad1fe4c2b516b204c62a6
Matous Jobanek <mjobanek@redhat.com>
2019-11-14 11:16:03 +0100

    chore: deploy csv for every commit in master via travis-ci (#94)

--- make/find-tools.mk
+++ make/find-tools.mk
@@ -6,13 +6,15 @@ REQUIRED_EXECUTABLES = go gofmt git oc operator-sdk sed yamllint find grep pytho
 ifneq (,$(findstring docker-,$(MAKECMDGOALS)))
     REQUIRED_EXECUTABLES = docker
 endif
-# Don't check for any tool if "make help" is run or "make" without a target.
+# Don't check for any tool if "make help" is run, "make" is executed without any target or SKIP_TOOLS var is set.
 ifneq ($(MAKECMDGOALS),help)
 ifneq ($(MAKECMDGOALS),)
+ifneq ($(SKIP_TOOLS),)
 ifeq ($(VERBOSE),1)
 $(info Searching for required executables: $(REQUIRED_EXECUTABLES)...)
 endif
 K := $(foreach exec,$(REQUIRED_EXECUTABLES),\
         $(if $(shell which $(exec) 2>/dev/null),some string,$(error "ERROR: No "$(exec)" binary found in in PATH!")))
+endif
 endif
 endif

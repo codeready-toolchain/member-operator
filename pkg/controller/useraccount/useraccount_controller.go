688308692b3b263531343e12383dde1c6ad2d859
tinakurian <tkurian@redhat.com>
2019-12-12 17:30:58 -0500

    removing unused import

--- pkg/controller/useraccount/useraccount_controller.go
+++ pkg/controller/useraccount/useraccount_controller.go
@@ -3,7 +3,6 @@ package useraccount
 import (
 	"context"
 	"fmt"
-	//logger "github.com/codeready-toolchain/registration-service/pkg/log"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/member-operator/pkg/config"

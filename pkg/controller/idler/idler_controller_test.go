48fd48e9ac2ae0c33d75bd3b3531621d17bd97d7
Alexey Kazakov <alkazako@redhat.com>
2020-07-17 14:59:01 -0700

    More tests

--- pkg/controller/idler/idler_controller_test.go
+++ pkg/controller/idler/idler_controller_test.go
@@ -3,8 +3,16 @@ package idler
 import (
 	"context"
 	"errors"
+	"fmt"
 	"reflect"
 	"testing"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	openshiftappsv1 "github.com/openshift/api/apps/v1"
+
+	appsv1 "k8s.io/api/apps/v1"
 
 	corev1 "k8s.io/api/core/v1"
 
@@ -16,6 +24,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -105,6 +114,61 @@ func TestEnsureIdling(t *testing.T) {
 	t.Run("Idle pods", func(t *testing.T) {
 
 	})
+}
+
+func preparePods(t *testing.T, r *ReconcileIdler, namespace string, startTime time.Time) {
+	podsToKill := make([]*corev1.Pod, 0)
+
+	sTime := metav1.NewTime(startTime)
+	replicas := int32(3)
+
+	// Deployment
+	d := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-deployment", Namespace: namespace},
+		Spec:       appsv1.DeploymentSpec{Replicas: &replicas},
+	}
+	rs := &appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-deployment-replicaset", Namespace: namespace},
+		Spec:       appsv1.ReplicaSetSpec{Replicas: &replicas},
+	}
+	controllerutil.SetControllerReference(d, rs, r.scheme)
+	podsToTrack := createPods(r, rs, sTime)
+
+	// TODO
+
+	// Standalone ReplicaSet
+	standalonRs := &appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-replicaset", Namespace: namespace},
+		Spec:       appsv1.ReplicaSetSpec{Replicas: &replicas},
+	}
+
+	ds := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-deamonset", Namespace: namespace},
+	}
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-statefulset", Namespace: namespace},
+	}
+	dc := &openshiftappsv1.DeploymentConfig{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-deploymentconfig", Namespace: namespace},
+		Spec:       openshiftappsv1.DeploymentConfigSpec{Replicas: replicas},
+	}
+	rc := &corev1.ReplicationController{
+		ObjectMeta: metav1.ObjectMeta{Name: namespace + "-deploymentconfig", Namespace: namespace},
+		Spec:       corev1.ReplicationControllerSpec{Replicas: &replicas},
+	}
+}
+
+func createPods(r *ReconcileIdler, owner v1.Object, startTime metav1.Time) []*corev1.Pod {
+	podsToTrack := make([]*corev1.Pod, 0, 3)
+	for i := 0; i < 3; i++ {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s-pod-%d", owner.GetName(), i), Namespace: owner.GetNamespace()},
+			Status:     corev1.PodStatus{StartTime: &startTime},
+		}
+		controllerutil.SetControllerReference(owner, pod, r.scheme)
+		podsToTrack = append(podsToTrack, pod)
+	}
+	return podsToTrack
 }
 
 func prepareReconcile(t *testing.T, name string, initObjs ...runtime.Object) (*ReconcileIdler, reconcile.Request, *test.FakeClient) {

89b1e27e60e79ef25a5ba269afd473d55c6756eb
Rajiv Senthilnathan <rsenthil@redhat.com>
2020-11-13 12:04:43 -0500

    Split read and write into two separate functions

--- pkg/che/che.go
+++ pkg/che/che.go
@@ -1,13 +1,8 @@
 package che
 
 import (
-	"context"
-	"fmt"
-
 	crtcfg "github.com/codeready-toolchain/member-operator/pkg/configuration"
 
-	routev1 "github.com/openshift/api/route/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -31,18 +26,4 @@ func InitDefaultCheClient(cfg *crtcfg.Config, cl client.Client) {
 		k8sClient:  cl,
 		tokenCache: newTokenCache(),
 	}
-}
-
-func getRouteURL(cl client.Client, namespace, name string) (string, error) {
-	route := &routev1.Route{}
-	namespacedName := types.NamespacedName{Namespace: namespace, Name: name}
-	err := cl.Get(context.TODO(), namespacedName, route)
-	if err != nil {
-		return "", err
-	}
-	scheme := "https"
-	if route.Spec.TLS == nil || *route.Spec.TLS == (routev1.TLSConfig{}) {
-		scheme = "http"
-	}
-	return fmt.Sprintf("%s://%s/%s", scheme, route.Spec.Host, route.Spec.Path), nil
 }
--- pkg/che/tokencache.go
+++ pkg/che/tokencache.go
@@ -15,6 +15,7 @@ import (
 
 	crtcfg "github.com/codeready-toolchain/member-operator/pkg/configuration"
 	"github.com/codeready-toolchain/member-operator/pkg/rest"
+	"github.com/codeready-toolchain/member-operator/pkg/utils"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/pkg/errors"
@@ -44,10 +45,9 @@ func (tc *tokenCache) getToken(cl client.Client, cfg *crtcfg.Config) (TokenSet, 
 		log.Info("Reusing token")
 		return *tc.token, nil
 	}
-
 	tc.RUnlock()
-	defer tc.Unlock()
-	tc.Lock()
+
+	// no valid token, retrieve a new one
 	// get the credentials
 	user, pass := cfg.GetCheAdminUsername(), cfg.GetCheAdminPassword()
 	if user == "" || pass == "" {
@@ -55,22 +55,20 @@ func (tc *tokenCache) getToken(cl client.Client, cfg *crtcfg.Config) (TokenSet, 
 	}
 
 	// get keycloak URL
-	cheKeycloakURL, err := getRouteURL(cl, cfg.GetCheNamespace(), cfg.GetCheKeycloakRouteName())
+	cheKeycloakURL, err := utils.GetRouteURL(cl, cfg.GetCheNamespace(), cfg.GetCheKeycloakRouteName())
 	if err != nil {
 		return TokenSet{}, err
 	}
 	log.Info("Che Keycloak Route", "URL", cheKeycloakURL)
 
-	// get che user token
-	if err = tc.obtainNewToken(cheKeycloakURL+tokenPath, user, pass); err != nil {
-		return TokenSet{}, err
-	}
-
-	return *tc.token, nil
+	return tc.obtainNewToken(cheKeycloakURL+tokenPath, user, pass)
 }
 
-// obtainNewToken obtains an access token from the provided authentication URL and returns it, otherwise returns an error
-func (tc *tokenCache) obtainNewToken(authURL, user, pass string) error {
+// obtainNewToken obtains an access token from the provided authentication URL, updates the cache and returns the token, otherwise returns an error
+func (tc *tokenCache) obtainNewToken(authURL, user, pass string) (TokenSet, error) {
+	defer tc.Unlock()
+	tc.Lock()
+
 	reqData := url.Values{}
 	reqData.Set("username", user)
 	reqData.Set("password", pass)
@@ -80,28 +78,30 @@ func (tc *tokenCache) obtainNewToken(authURL, user, pass string) error {
 	log.Info("Obtaining new token", "URL", authURL)
 	res, err := tc.httpClient.PostForm(authURL, reqData)
 	if err != nil {
-		return err
+		return TokenSet{}, err
 	}
 
 	defer rest.CloseResponse(res)
 	if res.StatusCode != http.StatusOK {
 		bodyString := rest.ReadBody(res.Body)
-		return errors.Errorf("unable to obtain access token for che, Response status: %s. Response body: %s", res.Status, bodyString)
+		return TokenSet{}, errors.Errorf("unable to obtain access token for che, Response status: %s. Response body: %s", res.Status, bodyString)
 	}
 	tokenSet, err := readTokenSet(res)
 	if err != nil {
-		return err
+		return TokenSet{}, err
 	}
 	if tokenSet.AccessToken == "" {
-		return errors.New("unable to obtain access token for che. Access Token is missing in the response")
+		return TokenSet{}, errors.New("unable to obtain access token for che. Access Token is missing in the response")
 	}
 	if tokenSet.Expiration == 0 && tokenSet.ExpiresIn > 0 {
 		timeLeft := time.Duration(tokenSet.ExpiresIn) * time.Second
 		tokenSet.Expiration = time.Now().Add(timeLeft).Unix()
 	}
 	log.Info("Token expiry", "expiration", tokenSet.Expiration, "expires_in", tokenSet.ExpiresIn)
+
+	// update token cache
 	tc.token = tokenSet
-	return nil
+	return *tc.token, nil
 }
 
 // TokenSet represents a set of Access and Refresh tokens

db27fd33a9420e9e519acb42ef6826a3d0f87ac8
Matous Jobanek <mjobanek@redhat.com>
2021-09-03 14:58:47 +0200

    status

--- controllers/useraccount/useraccount_controller.go
+++ controllers/useraccount/useraccount_controller.go
@@ -313,8 +313,8 @@ func (r *Reconciler) ensureNSTemplateSet(logger logr.Logger, userAcc *toolchainv
 
 	// update status if ready=false
 	readyCond, found := condition.FindConditionByType(nsTmplSet.Status.Conditions, toolchainv1alpha1.ConditionReady)
-	if !found || readyCond.Status == corev1.ConditionUnknown || (readyCond.Status == corev1.ConditionFalse && readyCond.Message == "") {
-		logger.Info("NSTemplateSet is either being provisioned or in an invalid state", "ready-condition", readyCond)
+	if !found || readyCond.Status == corev1.ConditionUnknown {
+		logger.Info("NSTemplateSet is in an invalid state", "ready-condition", readyCond)
 		return nsTmplSet, true, nil
 	}
 	if readyCond.Status == corev1.ConditionFalse {

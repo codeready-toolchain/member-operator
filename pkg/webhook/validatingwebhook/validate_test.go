b471fb73e874794d457ec6ffaa281a8716fe0541
Kanika Rana <krana@redhat.com>
2021-12-22 16:14:34 +0530

    testing rolebinding creation allowed

--- pkg/webhook/validatingwebhook/validate_test.go
+++ pkg/webhook/validatingwebhook/validate_test.go
@@ -39,12 +39,27 @@ func TestValidate(t *testing.T) {
 	verifyRequestBlocked(t, response)
 }
 
+func TestValidateAllow(t *testing.T) {
+	// when user is kubeadmin
+	response := validate(allowedJSON)
+
+	// then
+	verifyRequestAllowed(t, response)
+}
+
 func verifyRequestBlocked(t *testing.T, response []byte) {
 	reviewResponse := toReviewResponse(t, response)
 	assert.False(t, reviewResponse.Allowed)
 	assert.NotEmpty(t, reviewResponse.Result)
 	assert.Contains(t, reviewResponse.Result.Message, "trying to give access which is restricted")
 	assert.Equal(t, "a68769e5-d817-4617-bec5-90efa2bad6f6", string(reviewResponse.UID))
+}
+
+func verifyRequestAllowed(t *testing.T, response []byte) {
+	reviewResponse := toReviewResponse(t, response)
+	assert.True(t, reviewResponse.Allowed)
+	assert.Empty(t, reviewResponse.Result)
+	assert.Equal(t, "a68769e5-d817-4617-bec5-90efa2bad6g7", string(reviewResponse.UID))
 }
 
 func toReviewResponse(t *testing.T, content []byte) *v1.AdmissionResponse {
@@ -84,6 +99,72 @@ var rawJSON = []byte(`{
 		"operation": "CREATE",
 		"userInfo": {
 			"username": "johnsmith",
+			"groups": [
+				"system:authenticated"
+			],
+			"extra": {
+				"scopes.authorization.openshift.io": [
+					"user:full"
+				]
+			}
+		},
+		"object": {
+			"kind": "RoleBinding",
+			"apiVersion": "v1",
+			"metadata": {
+				"name": "busybox1",
+				"namespace": "johnsmith-dev"
+			},
+			"subjects": [{
+				"kind": "Group",
+				"name": "system:serviceaccounts",
+				"apiGroup": "rbac.authorization.k8s.io"
+			}],
+			"roleRef": {
+				"kind": "Role",
+				"apiGroup": "rbac.authorization.k8s.io",
+				"name": "rbac-edit"
+			}
+		},
+		"oldObject": null,
+		"dryRun": false,
+		"options": {
+			"kind": "CreateOptions",
+			"apiVersion": "meta.k8s.io/v1"
+		}
+	}
+}`)
+
+var allowedJSON = []byte(`{
+	"kind": "AdmissionReview",
+	"apiVersion": "admission.k8s.io/v1",
+	"request": {
+		"uid": "a68769e5-d817-4617-bec5-90efa2bad6g7",
+		"kind": {
+			"group": "",
+			"version": "v1",
+			"kind": "RoleBinding"
+		},
+		"resource": {
+			"group": "",
+			"version": "v1",
+			"resource": "rolebindings"
+		},
+		"requestKind": {
+			"group": "",
+			"version": "v1",
+			"kind": "RoleBinding"
+		},
+		"requestResource": {
+			"group": "",
+			"version": "v1",
+			"resource": "rolebindings"
+		},
+		"name": "busybox1",
+		"namespace": "johnsmith-dev",
+		"operation": "CREATE",
+		"userInfo": {
+			"username": "system:kubeadmin",
 			"groups": [
 				"system:authenticated"
 			],

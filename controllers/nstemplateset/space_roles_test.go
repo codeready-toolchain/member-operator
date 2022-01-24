87b2b97ab99a0a9137983f7a2983bd2a29eeb05a
Xavier Coulon <xcoulon@redhat.com>
2022-01-24 14:05:56 +0100

    test NSTemplateSet status
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- controllers/nstemplateset/space_roles_test.go
+++ controllers/nstemplateset/space_roles_test.go
@@ -44,6 +44,9 @@ func TestEnsureSpaceRoles(t *testing.T) {
 			// then
 			require.NoError(t, err)
 			assert.True(t, createdOrUpdated)
+			// at this point, the NSTemplateSet is still in `updating` state
+			AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+				HasConditions(Updating())
 			// verify that roles and rolebindings were created
 			AssertThatRole(t, "oddity-appstudio", "space-admin", memberClient).
 				Exists(). // created
@@ -89,7 +92,10 @@ func TestEnsureSpaceRoles(t *testing.T) {
 				// then
 				require.NoError(t, err)
 				assert.True(t, createdOrUpdated) // outdated objs deleted
-				// verify that `admin` role and rolebinding for `user1` and `user2` were deleted, but nothing changed for the viewer role and rolebindings
+				// at this point, the NSTemplateSet is still in `updating` state
+				AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+					HasConditions(Updating())
+					// verify that `admin` role and rolebinding for `user1` and `user2` were deleted, but nothing changed for the viewer role and rolebindings
 				AssertThatRole(t, "oddity-appstudio", "space-admin", memberClient).DoesNotExist()              // deleted
 				AssertThatRoleBinding(t, "oddity-appstudio", "user1-space-admin", memberClient).DoesNotExist() // deleted
 				AssertThatRoleBinding(t, "oddity-appstudio", "user2-space-admin", memberClient).DoesNotExist() // deleted
@@ -123,7 +129,10 @@ func TestEnsureSpaceRoles(t *testing.T) {
 				// then
 				require.NoError(t, err)
 				assert.True(t, createdOrUpdated)
-				// verify that role bindings for `user1` and `user2`` still exist and thet one was added for `user3`
+				// at this point, the NSTemplateSet is still in `updating` state
+				AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+					HasConditions(Updating())
+					// verify that role bindings for `user1` and `user2`` still exist and thet one was added for `user3`
 				AssertThatRole(t, "oddity-basic", "space-admin", memberClient).Exists()              // unchanged
 				AssertThatRoleBinding(t, "oddity-basic", "user1-space-admin", memberClient).Exists() // unchanged
 				AssertThatRoleBinding(t, "oddity-basic", "user2-space-admin", memberClient).Exists() // unchanged
@@ -151,6 +160,9 @@ func TestEnsureSpaceRoles(t *testing.T) {
 				// then
 				require.NoError(t, err)
 				assert.True(t, createdOrUpdated) // outdated objs deleted
+				// at this point, the NSTemplateSet is still in `updating` state
+				AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+					HasConditions(Updating())
 				// verify that rolebinding for `user1` was removed but the one for `user2` was not changed
 				AssertThatRole(t, "oddity-basic", "space-admin", memberClient).Exists()                    // unchanged
 				AssertThatRoleBinding(t, "oddity-basic", "user1-space-admin", memberClient).DoesNotExist() // deleted
@@ -174,7 +186,10 @@ func TestEnsureSpaceRoles(t *testing.T) {
 				createdOrUpdated, err := mgr.ensure(logger, nsTmplSet)
 				require.NoError(t, err)
 
-				// then verify that roles and rolebindings still exist, but nothing changed
+				// at this point, the NSTemplateSet is still in `updating` state
+				AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+					HasConditions(Updating())
+					// then verify that roles and rolebindings still exist, but nothing changed
 				assert.False(t, createdOrUpdated)
 				AssertThatRole(t, "oddity-basic", "space-admin", memberClient).Exists()              // created
 				AssertThatRoleBinding(t, "oddity-basic", "user1-space-admin", memberClient).Exists() // created
@@ -189,12 +204,12 @@ func TestEnsureSpaceRoles(t *testing.T) {
 		t.Run("error while listing namespaces", func(t *testing.T) {
 			// given
 			nsTmplSet := newNSTmplSet(commontest.MemberOperatorNs, "oddity", "basic", withNamespaces("abcde11", "basic"))
-			mgr, cl := prepareSpaceRolesManager(t, nsTmplSet)
-			cl.MockList = func(ctx context.Context, list runtimeclient.ObjectList, opts ...runtimeclient.ListOption) error {
+			mgr, memberClient := prepareSpaceRolesManager(t, nsTmplSet)
+			memberClient.MockList = func(ctx context.Context, list runtimeclient.ObjectList, opts ...runtimeclient.ListOption) error {
 				if _, ok := list.(*corev1.NamespaceList); ok {
 					return fmt.Errorf("mock error")
 				}
-				return cl.Client.List(ctx, list, opts...)
+				return memberClient.Client.List(ctx, list, opts...)
 			}
 
 			// when
@@ -202,6 +217,10 @@ func TestEnsureSpaceRoles(t *testing.T) {
 
 			// then
 			assert.EqualError(t, err, "failed to list namespaces for workspace 'oddity': mock error")
+			// at this point, the NSTemplateSet is still in `updating` state
+			AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+				HasConditions(UnableToProvision("mock error"))
+
 		})
 
 		t.Run("tiertemplate not found", func(t *testing.T) {
@@ -212,12 +231,14 @@ func TestEnsureSpaceRoles(t *testing.T) {
 					"admin-unknown-abcde11": {"user1", "user2"},
 				}))
 			ns := newNamespace(nsTmplSet.Spec.TierName, "oddity", "unknown")
-			mgr, _ := prepareSpaceRolesManager(t, nsTmplSet, ns)
+			mgr, memberClient := prepareSpaceRolesManager(t, nsTmplSet, ns)
 			// when
 			_, err := mgr.ensure(logger, nsTmplSet)
 
 			// then
 			assert.EqualError(t, err, "failed to retrieve space roles to apply: unable to retrieve the TierTemplate 'admin-unknown-abcde11' from 'Host' cluster: tiertemplates.toolchain.dev.openshift.com \"admin-unknown-abcde11\" not found")
+			AssertThatNSTemplateSet(t, commontest.MemberOperatorNs, "oddity", memberClient).
+				HasConditions(UpdateFailed(`unable to retrieve the TierTemplate 'admin-unknown-abcde11' from 'Host' cluster: tiertemplates.toolchain.dev.openshift.com "admin-unknown-abcde11" not found`))
 		})
 
 	})

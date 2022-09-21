8e4394252f347bc4473c2d417d8deaf9ba323b61
Xavier Coulon <xcoulon@redhat.com>
2022-09-21 09:58:04 +0200

    webhook: prevent users from creating CheCluster resources (#377)
    causes troubles in CRW when multiple `CheCluster` resources co-exist on
    the cluster
    
    also, configure webhook logger to produce JSON content
    also, move `klogWriter` into its own pkg to share between
    operator and webhook
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- main.go
+++ main.go
@@ -13,22 +13,23 @@ import (
 	"github.com/codeready-toolchain/member-operator/controllers/useraccount"
 	"github.com/codeready-toolchain/member-operator/pkg/apis"
 	"github.com/codeready-toolchain/member-operator/pkg/che"
+	"github.com/codeready-toolchain/member-operator/pkg/klog"
 	"github.com/codeready-toolchain/member-operator/version"
 	"github.com/codeready-toolchain/toolchain-common/controllers/toolchaincluster"
 	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
 	commonconfig "github.com/codeready-toolchain/toolchain-common/pkg/configuration"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/scale"
 
 	"go.uber.org/zap/zapcore"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/scale"
 	klogv1 "k8s.io/klog"
 	klogv2 "k8s.io/klog/v2"
 	metrics "k8s.io/metrics/pkg/apis/metrics/v1beta1"
@@ -121,7 +122,7 @@ func main() {
 		setupLog.Error(err, "")
 		os.Exit(1)
 	}
-	klogv1.SetOutputBySeverity("INFO", klogWriter{}) // tell klog v1 to use the custom writer
+	klogv1.SetOutputBySeverity("INFO", klog.Writer{}) // tell klog v1 to use the custom writer
 	// END : hack to redirect klogv1 calls to klog v2
 
 	printVersion()
@@ -338,33 +339,4 @@ func getCRTConfiguration(config *rest.Config) (membercfg.Configuration, error) {
 	}
 
 	return membercfg.GetConfiguration(cl)
-}
-
-// OutputCallDepth is the stack depth where we can find the origin of this call
-const OutputCallDepth = 6
-
-// DefaultPrefixLength is the length of the log prefix that we have to strip out
-const DefaultPrefixLength = 53
-
-// klogWriter is used in SetOutputBySeverity call below to redirect
-// any calls to klogv1 to end up in klogv2
-type klogWriter struct{}
-
-func (kw klogWriter) Write(p []byte) (n int, err error) {
-	if len(p) < DefaultPrefixLength {
-		klogv2.InfoDepth(OutputCallDepth, string(p))
-		return len(p), nil
-	}
-	if p[0] == 'I' {
-		klogv2.InfoDepth(OutputCallDepth, string(p[DefaultPrefixLength:]))
-	} else if p[0] == 'W' {
-		klogv2.WarningDepth(OutputCallDepth, string(p[DefaultPrefixLength:]))
-	} else if p[0] == 'E' {
-		klogv2.ErrorDepth(OutputCallDepth, string(p[DefaultPrefixLength:]))
-	} else if p[0] == 'F' {
-		klogv2.FatalDepth(OutputCallDepth, string(p[DefaultPrefixLength:]))
-	} else {
-		klogv2.InfoDepth(OutputCallDepth, string(p[DefaultPrefixLength:]))
-	}
-	return len(p), nil
 }
---
+++ pkg/webhook/validatingwebhook/validate_checluster_request.go
@@ -0,0 +1,73 @@
+package validatingwebhook
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+
+	userv1 "github.com/openshift/api/user/v1"
+	"github.com/pkg/errors"
+	admissionv1 "k8s.io/api/admission/v1"
+	"k8s.io/apimachinery/pkg/types"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type CheClusterRequestValidator struct {
+	Client runtimeClient.Client
+}
+
+func (v CheClusterRequestValidator) HandleValidate(w http.ResponseWriter, r *http.Request) {
+	var respBody []byte
+	body, err := ioutil.ReadAll(r.Body)
+	defer func() {
+		if err := r.Body.Close(); err != nil {
+			log.Error(err, "unable to close the body")
+		}
+	}()
+	if err != nil {
+		log.Error(err, "unable to read the body of the request")
+		w.WriteHeader(http.StatusInternalServerError)
+		respBody = []byte(fmt.Sprintf("unable to read the body of the request: %s", err))
+	} else {
+		// validate the request
+		respBody = v.validate(body)
+		w.WriteHeader(http.StatusOK)
+	}
+	if _, err := w.Write(respBody); err != nil {
+		log.Error(err, "unable to write response")
+	}
+}
+
+func (v CheClusterRequestValidator) validate(body []byte) []byte {
+	log.Info("incoming request", "body", string(body))
+	admReview := admissionv1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &admReview); err != nil {
+		log.Error(err, "unable to deserialize the admission review object", "body", string(body))
+		return denyAdmissionRequest(admReview, errors.Wrapf(err, "unable to deserialize the admission review object - body: %v", string(body)))
+	}
+	requestingUsername := admReview.Request.UserInfo.Username
+	// allow admission request if the user is a system user
+	if strings.HasPrefix(requestingUsername, "system:") {
+		return allowAdmissionRequest(admReview)
+	}
+	//check if the requesting user is a sandbox user
+	requestingUser := &userv1.User{}
+	err := v.Client.Get(context.TODO(), types.NamespacedName{
+		Name: admReview.Request.UserInfo.Username,
+	}, requestingUser)
+
+	if err != nil {
+		log.Error(err, "unable to find the user requesting creation of the CheCluster resource", "username", admReview.Request.UserInfo.Username)
+		return denyAdmissionRequest(admReview, errors.New("unable to find the user requesting the  creation of the CheCluster resource"))
+	}
+	if requestingUser.GetLabels()[toolchainv1alpha1.ProviderLabelKey] == toolchainv1alpha1.ProviderLabelValue {
+		log.Info("sandbox user is trying to create a CheCluster", "AdmissionReview", admReview)
+		return denyAdmissionRequest(admReview, errors.New("this is a Dev Sandbox enforced restriction. you are trying to create a CheCluster resource, which is not allowed"))
+	}
+	// at this point, it is clear the user isn't a sandbox user, allow request
+	return allowAdmissionRequest(admReview)
+}

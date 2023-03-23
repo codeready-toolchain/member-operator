64df697811ade8836289c849e6519f33eacc4811
Shane Bryzak <sbryzak@gmail.com>
2023-03-24 09:59:12 +1000

    disable caching when building scripts

--- make/webconsole.mk
+++ make/webconsole.mk
@@ -5,4 +5,4 @@ DIST_DIR = $(WEBCONSOLE_SRC_DIR)/dist
 generate-webconsole-scripts:
 	mkdir -p $(DIST_DIR)
 	#rm -f $(DIST_DIR)/*
-	cd $(WEBCONSOLE_SRC_DIR);${IMAGE_BUILDER} build --volume $(DIST_DIR):/dist:z,U -f $(WEBCONSOLE_SRC_DIR)/Containerfile .
+	cd $(WEBCONSOLE_SRC_DIR);${IMAGE_BUILDER} build --no-cache --volume $(DIST_DIR):/dist:z,U -f $(WEBCONSOLE_SRC_DIR)/Containerfile .

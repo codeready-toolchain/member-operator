625912da6ca0b1beb205d5e4346d8b57453f2136
Feny Mehta <fbm3307@gmail.com>
2023-10-17 16:07:56 +0530

    Fixing Single Pod test
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- controllers/idler/idler_controller_test.go
+++ controllers/idler/idler_controller_test.go
@@ -583,6 +583,7 @@ func TestEnsureIdlingFailed(t *testing.T) {
 		require.EqualError(t, err, "failed to ensure idling 'john-dev': cannot set status to fail")
 	})
 }
+
 func TestAppNameTypeForControllers(t *testing.T) {
 
 	idler := &toolchainv1alpha1.Idler{
@@ -681,6 +682,53 @@ func TestAppNameTypeForControllers(t *testing.T) {
 	}
 }
 
+func TestAppNameTypeForInidividualPods(t *testing.T) {
+	//given
+	idler := &toolchainv1alpha1.Idler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "alex-stage",
+			Labels: map[string]string{
+				toolchainv1alpha1.SpaceLabelKey: "alex",
+			},
+		},
+		Spec: toolchainv1alpha1.IdlerSpec{TimeoutSeconds: 60},
+	}
+
+	t.Run("Test AppName/Type in notification", func(t *testing.T) {
+		namespaces := []string{"dev", "stage"}
+		usernames := []string{"alex"}
+		nsTmplSet := newNSTmplSet(test.MemberOperatorNs, "alex", "advanced", "abcde11", namespaces, usernames)
+		mur := newMUR("alex")
+		reconciler, req, cl, _ := prepareReconcile(t, idler.Name, getHostCluster, idler, nsTmplSet, mur)
+		idlerTimeoutPlusOneSecondAgo := time.Now().Add(-time.Duration(idler.Spec.TimeoutSeconds+1) * time.Second)
+		p := preparePayloadsSinglePod(t, reconciler, idler.Name, "todelete-", idlerTimeoutPlusOneSecondAgo).standalonePods[0]
+		// first reconcile to track pods
+		res, err := reconciler.Reconcile(context.TODO(), req)
+		assert.NoError(t, err)
+		assert.True(t, res.Requeue)
+
+		// second reconcile should delete pods and create notification
+		res, err = reconciler.Reconcile(context.TODO(), req)
+		//then
+		assert.NoError(t, err)
+		memberoperatortest.AssertThatIdler(t, idler.Name, cl).
+			HasConditions(memberoperatortest.Running(), memberoperatortest.IdlerNotificationCreated())
+		//check the notification is actually created
+		hostCl, _ := reconciler.GetHostCluster()
+		notification := &toolchainv1alpha1.Notification{}
+		err = hostCl.Client.Get(context.TODO(), types.NamespacedName{
+			Namespace: test.HostOperatorNs,
+			Name:      "alex-stage-idled",
+		}, notification)
+		require.NoError(t, err)
+		require.Equal(t, "alex@test.com", notification.Spec.Recipient)
+		require.Equal(t, "idled", notification.Labels[toolchainv1alpha1.NotificationTypeLabelKey])
+		require.Equal(t, "Pod", notification.Spec.Context["AppType"])
+		require.Equal(t, p.Name, notification.Spec.Context["AppName"])
+
+	})
+
+}
 func TestCreateNotification(t *testing.T) {
 	idler := &toolchainv1alpha1.Idler{
 		ObjectMeta: metav1.ObjectMeta{
@@ -1084,6 +1132,25 @@ func preparePayloads(t *testing.T, r *Reconciler, namespace, namePrefix string, 
 		deploymentConfig:      dc,
 		replicationController: standaloneRC,
 		job:                   job,
+	}
+}
+
+func preparePayloadsSinglePod(t *testing.T, r *Reconciler, namespace, namePrefix string, startTime time.Time) payloads {
+	sTime := metav1.NewTime(startTime)
+
+	// Pods with no owner.
+	standalonePods := make([]*corev1.Pod, 0, 1)
+	for i := 0; i < 1; i++ {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s%s-pod-%d", namePrefix, namespace, i), Namespace: namespace},
+			Status:     corev1.PodStatus{StartTime: &sTime},
+		}
+		standalonePods = append(standalonePods, pod)
+		err := r.AllNamespacesClient.Create(context.TODO(), pod)
+		require.NoError(t, err)
+	}
+	return payloads{
+		standalonePods: standalonePods,
 	}
 }
 

de55fe6120d0951c4375a9d205f971e181f7701b
Francisc Munteanu <fmuntean@redhat.com>
2024-04-27 12:20:08 +0200

    fix some logs

--- pkg/webhook/deploy/deployment.go
+++ pkg/webhook/deploy/deployment.go
@@ -81,14 +81,15 @@ func Delete(ctx context.Context, cl runtimeclient.Client, s *runtime.Scheme, nam
 			objName = oldName
 		}
 		// TODO --- end temporary migration step
+		log.Info(fmt.Sprintf("Seraching for object to delete: %s  name:%s namespace:%s", obj.GetObjectKind().GroupVersionKind(), objName, obj.GetNamespace()))
 		if err := cl.Get(ctx, types.NamespacedName{Namespace: obj.GetNamespace(), Name: objName}, unst); err != nil {
 			if !errors.IsNotFound(err) { // Ignore not found
 				return false, errs.Wrap(err, "cannot get webhook object")
 			}
 		} else {
-			log.Info(fmt.Sprintf("Deleting %s  name:%s namespace:%s of kind  ", obj.GetObjectKind().GroupVersionKind(), objName, obj.GetNamespace()))
+			log.Info(fmt.Sprintf("Deleting %s  name:%s namespace:%s", obj.GetObjectKind().GroupVersionKind(), objName, obj.GetNamespace()))
 			if err := cl.Delete(ctx, unst); err != nil {
-				return false, errs.Wrap(err, "cannot get webhook object")
+				return false, errs.Wrap(err, "cannot delete webhook object")
 			}
 			deleted = true
 		}

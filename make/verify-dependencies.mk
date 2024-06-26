f75a84c3e1cec25fc5b79c79e80c3f2146bd8279
Feny Mehta <fbm3307@gmail.com>
2024-06-26 12:01:41 +0530

    Adding lint check
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/verify-dependencies.mk
+++ make/verify-dependencies.mk
@@ -1,6 +1,6 @@
 .PHONY: verify-dependencies
 ## Runs commands to verify after the updated dependecies of toolchain-common/API(go mod replace), if the repo needs any changes to be made
-verify-dependencies: tidy vet build test
+verify-dependencies: tidy vet build test lint
 
 .PHONY: tidy
 tidy: 

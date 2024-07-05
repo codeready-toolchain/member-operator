8cdd546f331abfbd83042a7fe1b5a91cd300a17a
Alexey Kazakov <alkazako@redhat.com>
2024-07-05 12:03:41 -0700

    cleanup

--- controllers/nstemplateset/features_test.go
+++ controllers/nstemplateset/features_test.go
@@ -9,64 +9,62 @@ import (
 )
 
 func TestShouldCreate(t *testing.T) {
-	t.Run("return namespace whose revision is not set", func(t *testing.T) {
-		// given
-		tests := []struct {
-			name                  string
-			objFeature            string
-			nsTemplateSetFeatures string
-			expectedToBeCreated   bool
-		}{
-			{
-				name:                  "object with no feature annotation should be created when no features are enabled in NSTemplateSet",
-				objFeature:            "",
-				nsTemplateSetFeatures: "",
-				expectedToBeCreated:   true,
-			},
-			{
-				name:                  "object with no feature annotation should be created when some features are enabled in NSTemplateSet",
-				objFeature:            "",
-				nsTemplateSetFeatures: "feature-1,feature-2",
-				expectedToBeCreated:   true,
-			},
-			{
-				name:                  "object with a feature annotation should not be created when no features are enabled in NSTemplateSet",
-				objFeature:            "feature-1",
-				nsTemplateSetFeatures: "",
-				expectedToBeCreated:   false,
-			},
-			{
-				name:                  "object with a feature annotation should not be created when different features are enabled in NSTemplateSet",
-				objFeature:            "feature-1",
-				nsTemplateSetFeatures: "feature-2",
-				expectedToBeCreated:   false,
-			},
-			{
-				name:                  "object with a feature which is among enabled features should be created",
-				objFeature:            "feature-2",
-				nsTemplateSetFeatures: "feature-1,feature-2,feature-3",
-				expectedToBeCreated:   true,
-			},
-		}
-		for _, testRun := range tests {
-			t.Run(testRun.name, func(t *testing.T) {
-				// given
-				obj := objectWithFeature(testRun.objFeature)
-				nsTmplSet := newNSTmplSet("na", "na", "na")
-				if testRun.nsTemplateSetFeatures != "" {
-					nsTmplSet.Annotations = map[string]string{
-						toolchainv1alpha1.FeatureToggleNameAnnotationKey: testRun.nsTemplateSetFeatures,
-					}
+	// given
+	tests := []struct {
+		name                  string
+		objFeature            string
+		nsTemplateSetFeatures string
+		expectedToBeCreated   bool
+	}{
+		{
+			name:                  "object with no feature annotation should be created when no features are enabled in NSTemplateSet",
+			objFeature:            "",
+			nsTemplateSetFeatures: "",
+			expectedToBeCreated:   true,
+		},
+		{
+			name:                  "object with no feature annotation should be created when some features are enabled in NSTemplateSet",
+			objFeature:            "",
+			nsTemplateSetFeatures: "feature-1,feature-2",
+			expectedToBeCreated:   true,
+		},
+		{
+			name:                  "object with a feature annotation should not be created when no features are enabled in NSTemplateSet",
+			objFeature:            "feature-1",
+			nsTemplateSetFeatures: "",
+			expectedToBeCreated:   false,
+		},
+		{
+			name:                  "object with a feature annotation should not be created when different features are enabled in NSTemplateSet",
+			objFeature:            "feature-1",
+			nsTemplateSetFeatures: "feature-2",
+			expectedToBeCreated:   false,
+		},
+		{
+			name:                  "object with a feature which is among enabled features should be created",
+			objFeature:            "feature-2",
+			nsTemplateSetFeatures: "feature-1,feature-2,feature-3",
+			expectedToBeCreated:   true,
+		},
+	}
+	for _, testRun := range tests {
+		t.Run(testRun.name, func(t *testing.T) {
+			// given
+			obj := objectWithFeature(testRun.objFeature)
+			nsTmplSet := newNSTmplSet("na", "na", "na")
+			if testRun.nsTemplateSetFeatures != "" {
+				nsTmplSet.Annotations = map[string]string{
+					toolchainv1alpha1.FeatureToggleNameAnnotationKey: testRun.nsTemplateSetFeatures,
 				}
+			}
 
-				// when
-				should := shouldCreate(obj, nsTmplSet)
+			// when
+			should := shouldCreate(obj, nsTmplSet)
 
-				// then
-				assert.Equal(t, testRun.expectedToBeCreated, should)
-			})
-		}
-	})
+			// then
+			assert.Equal(t, testRun.expectedToBeCreated, should)
+		})
+	}
 }
 
 func objectWithFeature(feature string) runtimeclient.Object {

0c41419565f80b7d4e3d75035be94cafb6afff87
Alexey Kazakov <alkazako@redhat.com>
2024-07-05 12:00:15 -0700

    Update controllers/nstemplateset/namespaces.go
    Co-authored-by: Rajiv Senthilnathan <rajivnathan@gmail.com>

--- controllers/nstemplateset/namespaces.go
+++ controllers/nstemplateset/namespaces.go
@@ -426,7 +426,7 @@ func (r *namespacesManager) isUpToDateAndProvisioned(ctx context.Context, ns *co
 		}
 
 		// check the names of the roles and roleBindings as well
-		// ignore featured objects if the corresponding feature is not enabled in the NSTemplateSet
+		// ignore feature-enabled objects if the corresponding feature is not enabled in the NSTemplateSet
 		for _, role := range processedRoles {
 			// It's NOT up-to-date if NOT found but should be created OR found but should NOT be created.
 			if found, err := r.containsRole(roleList.Items, role, spacename); (found != shouldCreate(role, nsTmplSet)) || err != nil {

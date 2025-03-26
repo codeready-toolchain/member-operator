a83ddd370ff014e70a5cb437a8df1dfbcd407f57
Matous Jobanek <mjobanek@redhat.com>
2025-03-26 11:21:14 +0100

    improve idler predicate & mapper (#651)

--- controllers/idler/mapper.go
+++ controllers/idler/mapper.go
@@ -3,13 +3,13 @@ package idler
 import (
 	"context"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
-	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // MapPodToIdler maps the pod to the idler
-func MapPodToIdler(_ context.Context, obj runtimeclient.Object) []reconcile.Request {
+func MapPodToIdler(_ context.Context, obj *v1.Pod) []reconcile.Request {
 	return []reconcile.Request{{
 		// the idler should have the same name as the user's namespace
 		NamespacedName: types.NamespacedName{

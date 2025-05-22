75a469b388d7ef4824fc5be8c14a62abd4a85352
Feny Mehta <fbm3307@gmail.com>
2025-05-22 11:28:28 +0530

    KUBESAW-143: Add Support for Go Templates (#666)
    * KUBESAW-143: Add Support for Go Templates
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * Changes to cache
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * rebase mess cleanup
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * isome more mess cleanup
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * some cleanup
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * test
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * not required golabl var
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * test cases for ttr
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * golint
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * test cases and avoiding panic
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * remove var
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    ---------
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    Co-authored-by: Francisc Munteanu <fmuntean@redhat.com>
    Co-authored-by: Matous Jobanek <mjobanek@redhat.com>

---
+++ controllers/nstemplateset/tier_template_revision.go
@@ -0,0 +1,34 @@
+package nstemplateset
+
+import (
+	"context"
+	"fmt"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// getTierTemplateRevision gets the TierTemplateRevision resource from the host cluster.
+func getTierTemplateRevision(ctx context.Context, hostClusterFunc cluster.GetHostClusterFunc, templateRef string) (*toolchainv1alpha1.TierTemplateRevision, error) {
+	// retrieve the ToolchainCluster instance representing the host cluster
+	host, ok := hostClusterFunc()
+	if !ok {
+		return nil, fmt.Errorf("unable to connect to the host cluster: unknown cluster")
+	}
+	if !cluster.IsReady(host.ClusterStatus) {
+		return nil, fmt.Errorf("the host cluster is not ready")
+	}
+
+	tierTemplateRevision := &toolchainv1alpha1.TierTemplateRevision{}
+	err := host.Client.Get(ctx, types.NamespacedName{
+		Namespace: host.OperatorNamespace,
+		Name:      templateRef,
+	}, tierTemplateRevision)
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to retrieve the TierTemplateRevision '%s' from 'Host' cluster", templateRef)
+	}
+	return tierTemplateRevision, nil
+}

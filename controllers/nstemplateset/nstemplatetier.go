f142bcfb35d5a2d21fcab25a00384ed70b12dd27
Feny Mehta <fbm3307@gmail.com>
2025-08-18 14:21:59 +0530

    KUBESAW-143: Add support for Go Templates in member operator (#680)
    * KUBESAW-143: Add support for Go Templates in member operator
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * updatingg test case
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * append and return
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * improving the code
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * reviewcomments
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * adding filter for goprocess and test cases
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * Review comments and golint
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * using individual scheme instead of global
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * removing extra test case
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * structuring the test case better
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * review comments
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * go lint
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * coderabbit changes, to add missing key=error for better error handling
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    ---------
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- controllers/nstemplateset/nstemplatetier.go
+++ controllers/nstemplateset/nstemplatetier.go
@@ -1,17 +1,22 @@
 package nstemplateset
 
 import (
+	"bytes"
 	"context"
+	"maps"
+
 	"fmt"
 
-	"github.com/codeready-toolchain/member-operator/pkg/host"
-	"github.com/codeready-toolchain/toolchain-common/pkg/configuration"
+	gotemp "text/template"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/member-operator/pkg/host"
+	"github.com/codeready-toolchain/toolchain-common/pkg/configuration"
 	"github.com/codeready-toolchain/toolchain-common/pkg/template"
 	templatev1 "github.com/openshift/api/template/v1"
 	"github.com/pkg/errors"
 	errs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -95,8 +100,15 @@ const (
 )
 
 // process processes the template inside of the tierTemplate object with the given parameters.
+// it first checks if tiertemplaterevision resource is present, and process its object and
+// if not present then it process the openshift template(current) logic
 // Optionally, it also filters the result to return a subset of the template objects.
 func (t *tierTemplate) process(scheme *runtime.Scheme, params map[string]string, filters ...template.FilterFunc) ([]runtimeclient.Object, error) {
+	//check if tiertemplaterevision is present then return the runtimeclient object of ttr
+	if t.ttr != nil {
+		return t.processGoTemplates(params, filters...)
+	}
+	// if ttr is not present then process the openshift template
 	ns, err := configuration.GetWatchNamespace()
 	if err != nil {
 		return nil, err
@@ -104,4 +116,62 @@ func (t *tierTemplate) process(scheme *runtime.Scheme, params map[string]string,
 	tmplProcessor := template.NewProcessor(scheme)
 	params[MemberOperatorNS] = ns // add (or enforce)
 	return tmplProcessor.Process(t.template.DeepCopy(), params, filters...)
+
+}
+
+// convert ttr parameters to a map
+func (t *tierTemplate) convertParametersToMap(runtimeParam map[string]string) map[string]string {
+	staticParamMap := map[string]string{}
+	for _, params := range t.ttr.Spec.Parameters {
+		staticParamMap[params.Name] = params.Value
+	}
+	maps.Copy(staticParamMap, runtimeParam) // need to add dynamic parameters like space-name also
+	return staticParamMap
+
+}
+
+func (t *tierTemplate) processGoTemplates(runtimeParams map[string]string, filters ...template.FilterFunc) ([]runtimeclient.Object, error) {
+	objList := make([]runtimeclient.Object, 0, len(t.ttr.Spec.TemplateObjects))
+	rawExtObjList := make([]runtime.RawExtension, 0, len(t.ttr.Spec.TemplateObjects))
+	paramMap := t.convertParametersToMap(runtimeParams) // go execute requires parameters in form of map
+
+	for i := range t.ttr.Spec.TemplateObjects {
+		var b bytes.Buffer
+		unStruct := unstructured.Unstructured{}
+		strTemp := string(t.ttr.Spec.TemplateObjects[i].Raw)
+
+		ttrTemp, err := gotemp.New(t.ttr.Name).Option("missingkey=error").Parse(strTemp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse go template for object %d in tierTemplateRevision %q: %w; raw: %q", i, t.ttr.Name, err, strTemp)
+		}
+
+		if err := ttrTemp.Execute(&b, paramMap); err != nil {
+			return nil, fmt.Errorf("failed to execute go template for object %d in tierTemplateRevision %q: %w; raw: %q", i, t.ttr.Name, err, strTemp)
+		}
+
+		runObj, _, err := unstructured.UnstructuredJSONScheme.Decode(b.Bytes(), nil, &unStruct)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode go template for object %d in tierTemplateRevision %q: %w; raw: %q", i, t.ttr.Name, err, strTemp)
+		}
+
+		// Convert runObj to runtime.RawExtension
+		// runtime.RawExtension expects an Object field, not Raw bytes
+		rawExt := runtime.RawExtension{
+			Object: runObj,
+		}
+		rawExtObjList = append(rawExtObjList, rawExt)
+	}
+	// Apply filters if needed
+	filtered := template.Filter(rawExtObjList, filters...)
+
+	// Convert filtered RawExtensions back to runtime.Objects
+	for _, f := range filtered {
+		if clientObj, ok := f.Object.(runtimeclient.Object); ok {
+			objList = append(objList, clientObj)
+		} else {
+			return nil, fmt.Errorf("unable to cast object to client.Object: %+v", f.Object)
+		}
+	}
+
+	return objList, nil
 }

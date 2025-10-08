a469926ee844fe608d72f259fc2ef304238e1c7e
Lukas Krejci <lkrejci@redhat.com>
2025-10-08 16:42:46 +0200

    Make the logic in clusterResourceManager.ensure() more digestable
    by splitting it up into more functions and structs.

--- controllers/nstemplateset/cluster_resources.go
+++ controllers/nstemplateset/cluster_resources.go
@@ -27,121 +27,71 @@ type clusterResourcesManager struct {
 // resource. It is assumed that if a cluster-scoped resource should be common to more than 1 nstemplateset, it should
 // be deployed externally from Devsandbox, e.g. using ArgoCD.
 func (r *clusterResourcesManager) ensure(ctx context.Context, nsTmplSet *toolchainv1alpha1.NSTemplateSet) error {
-	logger := log.FromContext(ctx)
-	userTierLogger := logger.WithValues("spacename", nsTmplSet.GetName(), "tier", nsTmplSet.Spec.TierName)
-	userTierCtx := log.IntoContext(ctx, userTierLogger)
-	userTierLogger.Info("ensuring cluster resources")
+	logger := log.FromContext(ctx, "spacename", nsTmplSet.GetName(), "tier", nsTmplSet.Spec.TierName)
+	logger.Info("ensuring cluster resources")
+	ctx = log.IntoContext(ctx, logger)
 
-	spacename := nsTmplSet.GetName()
-
-	var oldTemplateRef, newTemplateRef string
-	if nsTmplSet.Spec.ClusterResources != nil {
-		newTemplateRef = nsTmplSet.Spec.ClusterResources.TemplateRef
-	}
-	if nsTmplSet.Status.ClusterResources != nil {
-		oldTemplateRef = nsTmplSet.Status.ClusterResources.TemplateRef
-	}
-
-	if oldTemplateRef == newTemplateRef && !featuresChanged(nsTmplSet) {
+	oldTemplateRef, newTemplateRef, changed := getOldAndNewTemplateRefsIfChanged(nsTmplSet)
+	if !changed {
 		return nil
 	}
 
-	var newTierTemplate *tierTemplate
-	var oldTierTemplate *tierTemplate
-	var err error
-	if newTemplateRef != "" {
-		newTierTemplate, err = getTierTemplate(ctx, r.GetHostClusterClient, newTemplateRef)
-		if err != nil {
-			return r.wrapErrorWithStatusUpdateForClusterResourceFailure(userTierCtx, nsTmplSet, err,
-				"failed to retrieve the TierTemplate for the to-be-applied cluster resources with the name '%s'", newTemplateRef)
-		}
-	}
-	if oldTemplateRef != "" {
-		oldTierTemplate, err = getTierTemplate(ctx, r.GetHostClusterClient, oldTemplateRef)
-		if err != nil {
-			return r.wrapErrorWithStatusUpdateForClusterResourceFailure(userTierCtx, nsTmplSet, err,
-				"failed to retrieve TierTemplate for the last-applied cluster resources with the name '%s'", oldTemplateRef)
-		}
+	newTierTemplate, newObjs, err := r.processTierTemplate(ctx, nsTmplSet.Spec.ClusterResources, nsTmplSet.Name)
+	if err != nil {
+		return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
+			"failed to process the template for the to-be-applied cluster resources with the name '%s'", newTemplateRef)
 	}
 
-	var newObjs, currentObjects []runtimeclient.Object
-	if newTierTemplate != nil {
-		newObjs, err = newTierTemplate.process(r.Scheme, map[string]string{SpaceName: spacename})
-		if err != nil {
-			return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
-				"failed to process template for the to-be-applied cluster resources with the name '%s'", newTemplateRef)
-		}
-	}
-	if oldTierTemplate != nil {
-		currentObjects, err = oldTierTemplate.process(r.Scheme, map[string]string{SpaceName: spacename})
-		if err != nil {
-			return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
-				"failed to process template for the last-applied cluster resources with the name '%s'", oldTemplateRef)
-		}
+	_, curObjs, err := r.processTierTemplate(ctx, nsTmplSet.Status.ClusterResources, nsTmplSet.Name)
+	if err != nil {
+		return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
+			"failed to process the template for the last-applied cluster resources with the name '%s'", oldTemplateRef)
 	}
 
-	statusChanged := false
-	firstDeployment := nsTmplSet.Status.ClusterResources == nil || nsTmplSet.Status.ClusterResources.TemplateRef == ""
-	var failureStatusReason statusUpdater
-	if firstDeployment {
-		failureStatusReason = r.setStatusClusterResourcesProvisionFailed
-	} else {
-		failureStatusReason = r.setStatusUpdateFailed
-	}
-
-	changeStatusIfNeeded := func() error {
-		if !statusChanged {
-			if firstDeployment {
-				err = r.setStatusProvisioningIfNotUpdating(ctx, nsTmplSet)
-			} else {
-				err = r.setStatusUpdatingIfNotProvisioning(ctx, nsTmplSet)
-			}
-			if err != nil {
-				return err
-			}
-			statusChanged = true
-		}
-		return nil
-	}
+	objectApplier := newObjectApplier(r, nsTmplSet, curObjs, newTierTemplate)
 
 	for _, newObj := range newObjs {
-		if !shouldCreate(newObj, nsTmplSet) {
-			continue
-		}
-
-		// by removing the new objects from the current objects, we are going to be left with the objects
-		// that should be removed from the cluster after we're done with this loop
-		for oldIdx, curObj := range currentObjects {
-			if commonclient.SameGVKandName(curObj, newObj) {
-				currentObjects = slices.Delete(currentObjects, oldIdx, oldIdx+1)
-				break
-			}
-		}
-
-		if err := changeStatusIfNeeded(); err != nil {
-			return err
-		}
-
-		// create or update the resource
-		if _, err = r.apply(ctx, nsTmplSet, newTierTemplate, newObj); err != nil {
-			err := fmt.Errorf("failed to apply changes to the cluster resource %s, %s: %w", newObj.GetName(), newObj.GetObjectKind().GroupVersionKind().String(), err)
-			return r.wrapErrorWithStatusUpdate(ctx, nsTmplSet, failureStatusReason, err, "failure while syncing cluster resources")
-		}
-	}
-
-	// what we're left with here is the list of currently existing objects that are no longer present in the template.
-	// we need to delete them
-	if len(currentObjects) > 0 {
-		if err := changeStatusIfNeeded(); err != nil {
+		if err = objectApplier.Apply(ctx, newObj); err != nil {
 			return err
 		}
 	}
 
-	if err := deleteObsoleteObjects(ctx, r.Client, currentObjects, nil); err != nil {
-		return r.wrapErrorWithStatusUpdate(ctx, nsTmplSet, failureStatusReason, err, "failure while syncing cluster resources")
+	return objectApplier.Cleanup(ctx)
+}
+
+func getOldAndNewTemplateRefsIfChanged(nstt *toolchainv1alpha1.NSTemplateSet) (oldTemplateRef string, newTemplateRef string, changed bool) {
+	if nstt.Spec.ClusterResources != nil {
+		newTemplateRef = nstt.Spec.ClusterResources.TemplateRef
+	}
+	if nstt.Status.ClusterResources != nil {
+		oldTemplateRef = nstt.Status.ClusterResources.TemplateRef
 	}
 
-	return nil
+	changed = oldTemplateRef != newTemplateRef || featuresChanged(nstt)
+
+	return oldTemplateRef, newTemplateRef, changed
+}
+
+func (r *clusterResourcesManager) processTierTemplate(ctx context.Context, clusterResources *toolchainv1alpha1.NSTemplateSetClusterResources, spacename string) (*tierTemplate, []runtimeclient.Object, error) {
+	if clusterResources == nil {
+		return nil, nil, nil
+	}
+
+	var tierTemplate *tierTemplate
+	var objs []runtimeclient.Object
+	if clusterResources.TemplateRef != "" {
+		var err error
+		tierTemplate, err = getTierTemplate(ctx, r.GetHostClusterClient, clusterResources.TemplateRef)
+		if err != nil {
+			return nil, nil, err
+		}
+		objs, err = tierTemplate.process(r.Scheme, map[string]string{SpaceName: spacename})
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	return tierTemplate, objs, nil
 }
 
 // apply creates or updates the given object with the set of toolchain labels. If the apply operation was successful, then it returns 'true, nil',
@@ -173,15 +123,10 @@ func (r *clusterResourcesManager) delete(ctx context.Context, nsTmplSet *toolcha
 		return nil
 	}
 
-	currentTierTemplate, err := getTierTemplate(ctx, r.GetHostClusterClient, nsTmplSet.Status.ClusterResources.TemplateRef)
+	_, currentObjects, err := r.processTierTemplate(ctx, nsTmplSet.Status.ClusterResources, nsTmplSet.Name)
 	if err != nil {
 		return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
-			"failed to read the existing cluster resources")
-	}
-	currentObjects, err := currentTierTemplate.process(r.Scheme, map[string]string{SpaceName: nsTmplSet.Name})
-	if err != nil {
-		return r.wrapErrorWithStatusUpdateForClusterResourceFailure(ctx, nsTmplSet, err,
-			"failed to list existing cluster resources")
+			"failed to process the existing cluster resources")
 	}
 
 	for _, toDelete := range currentObjects {
@@ -204,5 +149,101 @@ func (r *clusterResourcesManager) delete(ctx context.Context, nsTmplSet *toolcha
 			return r.wrapErrorWithStatusUpdate(ctx, nsTmplSet, r.setStatusTerminatingFailed, err, "failed to delete cluster resource '%s'", toDelete.GetName())
 		}
 	}
+	return nil
+}
+
+// objectApplier is a helper to the clusterResourcesManager.ensure() method.
+// A new instance can be obtained using the newObjectApplier() function.
+//
+// It takes in the current state of the NSTemplateSet and then processes one new object to apply
+// to the cluster at a time, updating the internal bookkeeping to be able to Cleanup() the old objects
+// after all the new objects have been applied.
+type objectApplier struct {
+	r                   *clusterResourcesManager
+	statusChanged       bool
+	firstDeployment     bool
+	failureStatusReason statusUpdater
+	nstt                *toolchainv1alpha1.NSTemplateSet
+	currentObjects      []runtimeclient.Object
+	newTierTemplate     *tierTemplate
+}
+
+func newObjectApplier(r *clusterResourcesManager, nstt *toolchainv1alpha1.NSTemplateSet, currentObjects []runtimeclient.Object, newTierTemplate *tierTemplate) *objectApplier {
+	firstDeployment := nstt.Status.ClusterResources == nil || nstt.Status.ClusterResources.TemplateRef == ""
+	var failureStatusReason statusUpdater
+
+	if firstDeployment {
+		failureStatusReason = r.setStatusClusterResourcesProvisionFailed
+	} else {
+		failureStatusReason = r.setStatusUpdateFailed
+	}
+
+	return &objectApplier{
+		r:                   r,
+		statusChanged:       false,
+		firstDeployment:     firstDeployment,
+		failureStatusReason: failureStatusReason,
+		nstt:                nstt,
+		currentObjects:      currentObjects,
+		newTierTemplate:     newTierTemplate,
+	}
+}
+
+func (oa *objectApplier) changeStatusIfNeeded(ctx context.Context) error {
+	if !oa.statusChanged {
+		var err error
+		if oa.firstDeployment {
+			err = oa.r.setStatusProvisioningIfNotUpdating(ctx, oa.nstt)
+		} else {
+			err = oa.r.setStatusUpdatingIfNotProvisioning(ctx, oa.nstt)
+		}
+		if err != nil {
+			return err
+		}
+		oa.statusChanged = true
+	}
+	return nil
+}
+
+func (oa *objectApplier) Apply(ctx context.Context, obj runtimeclient.Object) error {
+	if !shouldCreate(obj, oa.nstt) {
+		return nil
+	}
+
+	// by removing the new objects from the current objects, we are going to be left with the objects
+	// that should be removed from the cluster after we're done with this loop
+	for oldIdx, curObj := range oa.currentObjects {
+		if commonclient.SameGVKandName(curObj, obj) {
+			oa.currentObjects = slices.Delete(oa.currentObjects, oldIdx, oldIdx+1)
+			break
+		}
+	}
+
+	if err := oa.changeStatusIfNeeded(ctx); err != nil {
+		return err
+	}
+
+	// create or update the resource
+	if _, err := oa.r.apply(ctx, oa.nstt, oa.newTierTemplate, obj); err != nil {
+		err := fmt.Errorf("failed to apply changes to the cluster resource %s, %s: %w", obj.GetName(), obj.GetObjectKind().GroupVersionKind().String(), err)
+		return oa.r.wrapErrorWithStatusUpdate(ctx, oa.nstt, oa.failureStatusReason, err, "failure while syncing cluster resources")
+	}
+
+	return nil
+}
+
+func (oa *objectApplier) Cleanup(ctx context.Context) error {
+	// what we're left with here is the list of currently existing objects that are no longer present in the template.
+	// we need to delete them
+	if len(oa.currentObjects) > 0 {
+		if err := oa.changeStatusIfNeeded(ctx); err != nil {
+			return err
+		}
+	}
+
+	if err := deleteObsoleteObjects(ctx, oa.r.Client, oa.currentObjects, nil); err != nil {
+		return oa.r.wrapErrorWithStatusUpdate(ctx, oa.nstt, oa.failureStatusReason, err, "failure while syncing cluster resources")
+	}
+
 	return nil
 }

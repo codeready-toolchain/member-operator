6853c7eaff9b93eb1d07341d1711586374800a98
Rajiv Senthilnathan <rsenthil@redhat.com>
2026-02-23 17:36:02 -0500

    Validate username set in cloudinit config upon creation

--- pkg/webhook/validatingwebhook/validate_vm_request.go
+++ pkg/webhook/validatingwebhook/validate_vm_request.go
@@ -4,11 +4,13 @@ import (
 	"html"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
 )
 
 const manualRunStrategy = "Manual"
@@ -55,15 +57,98 @@ func (v VMRequestValidator) validate(body []byte) []byte {
 		return denyAdmissionRequest(admReview, errors.New("failed to validate VirtualMachine request"))
 	}
 
-	runStrategy, hasRunStrategy, err := unstructured.NestedString(unstructuredRequestObj.Object, "spec", "runStrategy")
-	if err != nil {
-		log.Error(err, "failed to unmarshal VirtualMachine json object", "AdmissionReview", admReview)
-		return denyAdmissionRequest(admReview, errors.New("failed to validate VirtualMachine request"))
+	// runStrategy is mutated to manual by the virtual machines mutating webhook for CREATE so it only needs to be blocked for UPDATE
+	if admReview.Request.Operation == admissionv1.Update {
+		runStrategy, hasRunStrategy, err := unstructured.NestedString(unstructuredRequestObj.Object, "spec", "runStrategy")
+		if err != nil {
+			log.Error(err, "failed to unmarshal VirtualMachine json object", "AdmissionReview", admReview)
+			return denyAdmissionRequest(admReview, errors.New("failed to validate VirtualMachine request"))
+		}
+		if hasRunStrategy && runStrategy != manualRunStrategy {
+			log.Info("sandbox user is trying to configure a VM with RunStrategy set to a value other than Manual", "AdmissionReview", admReview) // other run strategies are not allowed because it conflicts with the Dev Sandbox Idler
+			return denyAdmissionRequest(admReview, errors.New("this is a Dev Sandbox enforced restriction. Only 'Manual' RunStrategy is permitted"))
+		}
 	}
-	if hasRunStrategy && runStrategy != manualRunStrategy {
-		log.Info("sandbox user is trying to configure a VM with RunStrategy set to a value other than Manual", "AdmissionReview", admReview) // other run strategies are not allowed because it conflicts with the Dev Sandbox Idler
-		return denyAdmissionRequest(admReview, errors.New("this is a Dev Sandbox enforced restriction. Only 'Manual' RunStrategy is permitted"))
+
+	// validate that a username is configured in the cloudInit volume (only for CREATE operations since cloudInit is used for "first-boot" initialization)
+	if admReview.Request.Operation == admissionv1.Create {
+		if err := validateCloudInitUsername(unstructuredRequestObj); err != nil {
+			log.Error(err, "cloudInit username validation failed", "VirtualMachine", unstructuredRequestObj)
+			return denyAdmissionRequest(admReview, errors.New("this is a Dev Sandbox enforced restriction. A user must be configured in either the cloudInitNoCloud or cloudInitConfigDrive volume."))
+		}
 	}
+
 	// the user is configuring a VM without the 'runStrategy' configured or with it configured to Manual, allowing the request.
 	return allowAdmissionRequest(admReview)
+}
+
+// validateCloudInitUsername validates that a username is configured in the cloudInit volume
+func validateCloudInitUsername(unstructuredRequestObj *unstructured.Unstructured) error {
+	volumes, volumesFound, err := unstructured.NestedSlice(unstructuredRequestObj.Object, "spec", "template", "spec", "volumes")
+	if err != nil {
+		return errors.Wrap(err, "failed to get volumes from VirtualMachine")
+	}
+
+	if !volumesFound {
+		return errors.New("no volumes found in VirtualMachine")
+	}
+
+	// iterate through volumes to find cloudinitdisk
+	for _, volume := range volumes {
+		volumeDef, ok := volume.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		if volumeDef["name"] == "cloudinitdisk" {
+			// check supported cloudInit types
+			supportedCloudInitConfigTypes := []string{"cloudInitNoCloud", "cloudInitConfigDrive"}
+
+			for _, cloudInitType := range supportedCloudInitConfigTypes {
+				cloudInitConfig, found := volumeDef[cloudInitType].(map[string]interface{})
+				if !found {
+					continue
+				}
+
+				// check if userData contains a username
+				userData, ok := cloudInitConfig["userData"].(string)
+				if !ok {
+					// no userData found in this cloudInit config
+					continue
+				}
+
+				// parse the userData YAML and check for username field
+				if hasUsername(userData) {
+					return nil // username found, validation passed
+				}
+			}
+		}
+	}
+
+	return errors.New("no username configured in cloudInit volume")
+}
+
+// hasUsername checks if the userData contains a 'user' field
+func hasUsername(userDataString string) bool {
+	if !strings.HasPrefix(userDataString, "#cloud-config\n") {
+		log.Info("userData does not contain the required '#cloud-config' header", "userData", userDataString)
+		return false
+	}
+	userDataString = strings.TrimPrefix(userDataString, "#cloud-config\n")
+	userDataString = strings.TrimSpace(userDataString)
+
+	userData := map[string]interface{}{}
+	if err := yaml.Unmarshal([]byte(userDataString), &userData); err != nil {
+		log.Error(err, "failed to unmarshal userData YAML", "userData", userDataString)
+		return false
+	}
+
+	// check if 'user' field exists and is not empty
+	if user, found := userData["user"]; found {
+		if userStr, ok := user.(string); ok && userStr != "" {
+			return true
+		}
+	}
+
+	return false
 }
